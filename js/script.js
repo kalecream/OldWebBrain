87ae7a5962d0fc8715cff6d10e2abfbc62b5efe9
kalecream <sabrinamedwinter@gmail.com>
2022-09-23 13:53:39 -0500

    add color toggle

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+
+//  theme toggle
+const toggle = document.queryElementbyId("theme-toggle");
+toggle.addEventListener("click", toggleTheme);
+
+function toggleTheme() {
+    var body = document.body;
+    body.classList.toggle("dark");
+}
+
+//  math function
 function random_int(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
