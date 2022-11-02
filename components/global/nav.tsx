b44f9d0340353386309502bedfd0651eab9a745b
kalecream <sabrinamedwinter@gmail.com>
2022-11-02 13:56:43 -0500

    Update Sitename Style

--- components/global/nav.tsx
+++ components/global/nav.tsx
@@ -1,54 +1,58 @@
-import * as React from 'react';
-import styles from '../../styles/nav.module.css';
+import * as React from "react";
+import styles from "../../styles/nav.module.css";
 
 interface NavProps {
-    title: string;
-    links: string[];
-};
+  title: string;
+  links: string[];
+}
 
 const directory = [
-    {
-        title: 'About',
-        links: "/about"
-    },
-    {
-        title: 'Contact',
-        links: "/contact"
-    },
-    {
-        title: 'Rolodex',
-        links: "/rolodex"
-    },
-    {
-        title: 'Blog',
-        links: "/blog"
-    },
-    {
-        title: 'Portfolio',
-        links: "/portfolio"
-    }
+  {
+    title: "About",
+    links: "/about",
+  },
+  {
+    title: "Contact",
+    links: "/contact",
+  },
+  {
+    title: "Rolodex",
+    links: "/rolodex",
+  },
+  {
+    title: "Blog",
+    links: "/blog",
+  },
+  {
+    title: "Portfolio",
+    links: "/portfolio",
+  },
 ];
 
-export const Navigation: React.FunctionComponent<NavProps> = ({ title, links }) => {
-    function toggleTheme() {
-        var body = document.body;
-        body.classList.toggle("dark");
-    }
-    
-    return (
-        <nav className={styles.nav}>
-            <div id="site-name">KaleCream</div>
-            <ul>
-                {directory.map((directory, index) => (
-                    <li key={index}>
-                        <a href={directory.links}>{directory.title}</a>
-                    </li>
-                ))}
-            </ul>
-            <div className={styles.settings}>
-                <button id="theme-toggle" className={styles.themeToggle} >
-                </button>
-            </div>
-        </nav>
-    );
+export const Navigation: React.FunctionComponent<NavProps> = ({
+  title,
+  links,
+}) => {
+  function toggleTheme() {
+    var body = document.body;
+    body.classList.toggle("dark");
+  }
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.siteName}>
+        <a href="/">KaleCream</a>
+      </div>
+      <ul>
+        {directory.map((directory, index) => (
+          <li key={index}>
+            <a href={directory.links}>{directory.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className={styles.settings}>
+        <button id="theme-toggle" className={styles.themeToggle}></button>
+      </div>
+    </nav>
+  );
 };
