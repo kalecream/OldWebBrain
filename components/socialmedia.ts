404eee12c8736c901be661017e648dc5c2c704fd
kalecream <sabrinamedwinter@gmail.com>
2022-11-02 14:58:29 -0500

    Change to Next Typescript

---
+++ components/socialmedia.ts
@@ -0,0 +1,32 @@
+export const MediaProfiles = [
+    {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/yourname',
+        icon: 'facebook',
+        color: '#3b5998'
+    },
+    {
+        name: 'Twitter',
+        url: 'https://twitter.com/yourname',
+        icon: 'twitter',
+        color: '#00aced'
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/yourname',
+        icon: 'linkedin',
+        color: '#007bb6'
+    },
+    {  
+        name: 'GitHub',
+        url: '',
+        icon: 'github',
+        color: '#333333'
+    },
+    {
+        name: 'StackOverflow',
+        url: '',
+        icon: 'stack-overflow',
+        color: '#f48024'
+    }
+]
