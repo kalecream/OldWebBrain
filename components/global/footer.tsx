d9eeec9a8f6bd4cfcbbfbd9da9537435acc062df
kalecream <sabrinamedwinter@gmail.com>
2022-11-02 13:59:58 -0500

    remove contact from footer

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -6,10 +6,6 @@ export const Footer:React.FunctionComponent = () => {
       <p id="copyright">
         &copy; {new Date().getFullYear()} KaleCream LLC. All rights reserved.
       </p>
-      
-      <p id="contact">
-        <a href="mailto:sabrinamedwinter@gmail.com"> &nbsp; Contact Us</a>
-        </p>
     </footer>
   );
 }
