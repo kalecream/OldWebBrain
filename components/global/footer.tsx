f594f74c92a7148e7b701295d329c0518b66075b
kalecream <sabrinamedwinter@gmail.com>
2022-11-02 16:27:29 -0500

    Change company in footer

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -4,7 +4,7 @@ export const Footer:React.FunctionComponent = () => {
   return (
     <footer>
       <p id="copyright">
-        &copy; {new Date().getFullYear()} KaleCream LLC. All rights reserved.
+        &copy; {new Date().getFullYear()} KaleCream Limited. All rights reserved.
       </p>
     </footer>
   );
