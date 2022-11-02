4eb54568fd327a83b9e572c8c96fa7022662e341
kalecream <sabrinamedwinter@gmail.com>
2022-11-02 13:50:24 -0500

    navigation

--- components/global/nav.tsx
+++ components/global/nav.tsx
@@ -1,26 +1,54 @@
 import * as React from 'react';
 import styles from '../../styles/nav.module.css';
 
-export const Navigation:React.FunctionComponent = () => {
+interface NavProps {
+    title: string;
+    links: string[];
+};
+
+const directory = [
+    {
+        title: 'About',
+        links: "/about"
+    },
+    {
+        title: 'Contact',
+        links: "/contact"
+    },
+    {
+        title: 'Rolodex',
+        links: "/rolodex"
+    },
+    {
+        title: 'Blog',
+        links: "/blog"
+    },
+    {
+        title: 'Portfolio',
+        links: "/portfolio"
+    }
+];
+
+export const Navigation: React.FunctionComponent<NavProps> = ({ title, links }) => {
     function toggleTheme() {
         var body = document.body;
         body.classList.toggle("dark");
     }
-
-  return (
-    <nav>
-        <div id="site-name"><a href="./index.html">KaleCream</a></div>
-        <ul>
-            <li><a href="html/directory.html">Playground</a></li> ;
-            <li>Rolodex</li> ;
-            <li>Log</li> ;
-            <li><a href="html/blog.html">Blog</a></li> ;
-            <li>Contact</li> ;
-        </ul>
-        <div className={styles.settings}>
-            <button id="theme-toggle" className={styles.themeToggle} onClick="toggleTheme">
-            </button>
-        </div>
-    </nav>
-  );
-}
+    
+    return (
+        <nav className={styles.nav}>
+            <div id="site-name">KaleCream</div>
+            <ul>
+                {directory.map((directory, index) => (
+                    <li key={index}>
+                        <a href={directory.links}>{directory.title}</a>
+                    </li>
+                ))}
+            </ul>
+            <div className={styles.settings}>
+                <button id="theme-toggle" className={styles.themeToggle} >
+                </button>
+            </div>
+        </nav>
+    );
+};
