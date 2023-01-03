bef04a057a2ef50836dd31ab9822bd4711b49328
kalecream <sabrinamedwinter@gmail.com>
2023-01-03 07:37:42 -0500

    add Google Analytics

--- containers/layout/page.tsx
+++ containers/layout/page.tsx
@@ -1,9 +1,10 @@
-import * as React from "react";
+import { useEffect} from "react";
 import Head from "next/head";
 import { Navigation, Footer } from "../../components/global";
 import styles from "../../styles/Home.module.css";
 import { Icons } from "../../assets";
 import styled from "@emotion/styled";
+import TagManager from "react-gtm-module";
 
 const PageTitle = styled.h1`
 	font-size: 3rem;
@@ -20,11 +21,18 @@ export default function Page({
 	title?: string;
 	description?: string;
 }) {
+
+	useEffect(() => {
+		TagManager.initialize({ gtmId: "G-9K8N204BH7"});
+	}, []);
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>KaleCream {title ? "| " + title : null}</title>
 				<meta name="description" content={description} />
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-9K8N204BH7"></script>
+				
+
 			</Head>
 			<Navigation />
 			<main className={styles.main}>
@@ -32,6 +40,15 @@ export default function Page({
 				{children}
 			</main>
 			<Footer />
+			{
+				<script>
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'G-9K8N204BH7');
+				</script>
+			}
 		</div>
 	);
 }
