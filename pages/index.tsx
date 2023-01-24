116be04c45e6dc1ff1d371a2e2a575f5e32e9065
kalecream <sabrinamedwinter@gmail.com>
2023-01-23 22:49:40 -0500

    Add blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -178,20 +178,22 @@ export const Home = ({ posts }: IndexProps): JSX.Element => {
 				<h2>Blog</h2>
 				{posts.map((post) => (
 					<article key={post.slug} className="mt-12">
-						<p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-							{/* {format(parseISO(post.date), "MMMM dd, yyyy")} */}
-						</p>
+						{/* <p >
+							{format(parseISO(post.date), "MMMM dd, yyyy")}
+						</p><p >
+							{format(parseISO(post.date), "MMMM dd, yyyy")}
+						</p> */}
 						<h1 className="mb-2 text-xl">
-							<Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
-								{/* <a className="text-gray-900 dark:text-white dark:hover:text-blue-400">
+							<Link legacyBehavior as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+								<a className="text-gray-900 dark:text-white dark:hover:text-blue-400">
 									{post.title}
-								</a> */}
+								</a>
 							</Link>
 						</h1>
 						<p className="mb-3">{post.description}</p>
 						<p>
-							<Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
-								{/* <a>Read More</a> */}
+							<Link legacyBehavior as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+								<a>Read More</a>
 							</Link>
 						</p>
 					</article>
