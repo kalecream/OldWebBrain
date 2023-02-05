09a2d74cff5488050377110a0b0f4a3880d63558
kalecream <sabrinamedwinter@gmail.com>
2023-02-05 08:59:57 -0500

    ADD theming system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import Head from "next/head";
 import { ThemeProvider } from "next-themes";
 import { MetaProps } from "../types/layout";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { darkTheme, lightTheme, GlobalStyles } from "../theme/ThemeConfig";
 
 config.autoAddCss = false;
 export const WEBSITE_HOST_URL = "https://www.kalecream.com";
@@ -13,18 +15,42 @@ export const WEBSITE_HOST_URL = "https://www.kalecream.com";
 export const App = (
 	{ Component, pageProps }: AppProps,
 	{ customMeta }: { customMeta?: MetaProps }
-	): JSX.Element => {
-		const router = useRouter();
-		const meta: MetaProps = {
-			title: "KaleCream",
-			description: "Web Developer.",
-			image: `${WEBSITE_HOST_URL}/images/favicon.svg`,
-			type: "website",
-			...customMeta,
-		};
+): JSX.Element => {
+	const [theme, setTheme] = useState("light");
+	const router = useRouter();
+	const meta: MetaProps = {
+		title: "KaleCream",
+		description: "Web Developer.",
+		image: `${WEBSITE_HOST_URL}/images/favicon.svg`,
+		type: "website",
+		...customMeta,
+	};
+	let [newColorScheme, setColorScheme] = useState("light");
+
+	const toggleTheme = () => {
+		theme === "light" ? setTheme("dark") : setTheme("light");
+	};
+
+	// Getting the user's Theme preference from the browser
+	useEffect(() => {
+		window
+			.matchMedia("(prefers-color-scheme: dark)")
+			.addEventListener("change", (event) => {
+				event.matches ? setColorScheme("dark") : setColorScheme("light");
+			});
+	}, []);
+
+	// If colour scheme comes back void then return light theme
 
 	return (
-		<ThemeProvider attribute="class" enableSystem={true} defaultTheme="light">
+		<ThemeProvider
+			attribute="class"
+			enableSystem={true}
+			defaultTheme={newColorScheme ?? "light"}
+		>
+			{/* TODO: createGlobalStyles not functioning in webpack 5 */}
+			{/* <GlobalStyles /> */}
+			{/* <button onClick={toggleTheme}>Switch Theme</button> */}
 			<Head>
 				<title>{meta.title}</title>
 				<meta content={meta.description} name="description" />
@@ -42,7 +68,10 @@ export const App = (
 				<meta name="twitter:description" content={meta.description} />
 				<meta property="og:type" content={meta.type} />
 				<meta name="twitter:image" content={meta.image} />
-				<meta property="og:url" content={`${WEBSITE_HOST_URL}${router.asPath}`} />
+				<meta
+					property="og:url"
+					content={`${WEBSITE_HOST_URL}${router.asPath}`}
+				/>
 				<link rel="canonical" href={`${WEBSITE_HOST_URL}${router.asPath}`} />
 				{meta.date && (
 					<meta property="article:published_time" content={meta.date} />
