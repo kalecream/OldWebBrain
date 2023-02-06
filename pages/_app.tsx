fefc7059984bb4881b2f194f44fc1dadb7d2169f
kalecream <sabrinamedwinter@gmail.com>
2023-02-06 10:24:02 -0500

    REMOVE 3daily page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -58,16 +58,7 @@ export const App = (
 				{meta.date && (
 					<meta property="article:published_time" content={meta.date} />
 				)}
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossOrigin="true"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@100;200;300;400;500;600;700;900&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-					rel="stylesheet"
-				/>
+				
 			</Head>
 			<Component {...pageProps} />
 		</ThemeProvider>
