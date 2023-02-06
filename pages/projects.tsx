6a7cebed796a994794656bf0b97254ef6e4698df
kalecream <sabrinamedwinter@gmail.com>
2023-02-06 07:43:10 -0500

    FIX projects colors

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,20 +1,20 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import { Colors } from "../styles/colors";
 import Page from "../containers/layout/page";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faToolbox,
-	faLanguage
-} from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
-import { create } from "domain";
+import { faToolbox, faLanguage } from "@fortawesome/free-solid-svg-icons";
 import "animate.css";
-import { Section, Container, Card, CardTitle, Button } from "../components/global";
+import {
+	Section,
+	Container,
+	Card,
+	CardTitle,
+	Button,
+} from "../components/global";
 import Projects from "../data/projectsData";
 
 const ProjectDescription = styled.p`
-	color: ${Colors.neutral[200]};
+	color: var(--grey);
 	font-size: 0.85rem;
 `;
 
@@ -24,8 +24,8 @@ const RigidContainer = styled.div`
 	flex-direction: column;
 `;
 
-const ProjectDataPoint= styled.ul`
-	color: ${Colors.neutral[300]};
+const ProjectDataPoint = styled.ul`
+	color: var(--grey);
 	font-size: 0.85rem;
 	display: flex;
 	flex-wrap: wrap;
@@ -33,22 +33,21 @@ const ProjectDataPoint= styled.ul`
 	margin: 0.25rem 0;
 
 	& li {
-		display: grid;
-		place-items: center;
+		display: flex;
 		margin-top: 0.5rem;
 		margin-right: 0.5rem;
 	}
 
 	& li a:hover {
-		background-color: ${Colors.primary};
-		color: ${Colors.lightShade};
+		color: var(--background);
 	}
 
 	& li a {
-		background-color: ${Colors.lightShade};
+		background-color: var(--background);
 		padding: 0.1rem 0.4rem;
-		border-radius: 3px;
-		color: ${Colors.primary};
+		border-radius: 5px;
+		border: 1px solid var(--secondary);
+		color: var(--secondary);
 	}
 `;
 
@@ -67,31 +66,30 @@ export const Directory: any = () => {
 							<CardTitle>{project.title}</CardTitle>
 							<ProjectDescription>{project.description}</ProjectDescription>
 							<RigidContainer>
-							{project.technology ? (
-								<ProjectDataPoint title="Technologies">
-									<Emoji>
-										<FontAwesomeIcon icon={faToolbox} />
-									</Emoji>
-									{project.technology?.map((tech, index) => (
-										<li key={index}>{tech}</li>
-									))}
-								</ProjectDataPoint>
-							) : null}
-							{project.language ? (
-								<ProjectDataPoint>
-									<Emoji>
-										<FontAwesomeIcon icon={faLanguage} title="Languages" />
-									</Emoji>
-									{project.language?.map((lang, index) => (
-										<li key={index}>
-											<a title="Clicking this does nothing right now. Sorry.">
-												{lang}
-											</a>
-										</li>
-									))}
-								</ProjectDataPoint>
-							) : null}
-							
+								{project.technology ? (
+									<ProjectDataPoint title="Technologies">
+										<Emoji>
+											<FontAwesomeIcon icon={faToolbox} />
+										</Emoji>
+										{project.technology?.map((tech, index) => (
+											<li key={index}>{tech}</li>
+										))}
+									</ProjectDataPoint>
+								) : null}
+								{project.language ? (
+									<ProjectDataPoint>
+										<Emoji>
+											<FontAwesomeIcon icon={faLanguage} title="Languages" />
+										</Emoji>
+										{project.language?.map((lang, index) => (
+											<li key={index}>
+												<a title="Clicking this does nothing right now. Sorry.">
+													{lang}
+												</a>
+											</li>
+										))}
+									</ProjectDataPoint>
+								) : null}
 							</RigidContainer>
 							{project.link ? (
 								<Button href={project.link} target={"_blank"}>
