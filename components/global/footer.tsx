a79c6ec576cc687a7fc07047d09c117de320e3ee
kalecream <sabrinamedwinter@gmail.com>
2023-02-15 18:02:11 -0500

    fix: Unnessary Separator in Footer

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -1,49 +1,39 @@
 import styled from "@emotion/styled";
 import { CustomLink } from "./Basics";
-import { useEffect, useState } from "react";
 
 const StyledFooter = styled.footer`
-	bottom: 0;
-	height: 4rem;
-	font-size: 0.65rem;
-	opacity: 0.75;
-	display: flex;
-	justify-content: space-between;
+  bottom: 0;
+  height: 4rem;
+  font-size: 0.65rem;
+  opacity: 0.75;
+  display: flex;
+  justify-content: space-between;
 
-	@media screen and (max-width: 450px) {
-		margin: 0.5rem 0;
-		padding: 1rem 0;
-	}
+  @media screen and (max-width: 450px) {
+    margin: 0.5rem 0;
+    padding: 1rem 0;
+  }
 
-	@media screen and (max-width: 768px) {
-		margin: 0.05rem 1rem;
-		flex-wrap: wrap;
-		justify-content: center;
-	}
+  @media screen and (max-width: 768px) {
+    margin: 0.05rem 1rem;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 
-	@media screen and (max-width: 1024px) {
-		margin: 0.05rem 3rem;
-	}
+  @media screen and (max-width: 1024px) {
+    margin: 0.05rem 3rem;
+  }
 
-	@media screen and (min-width: 1024px) {
-		margin: 0.05rem 5rem;
-	}
+  @media screen and (min-width: 1024px) {
+    margin: 0.05rem 5rem;
+  }
 `;
 
 export const Footer: React.FunctionComponent = () => {
-	const [lastWebsiteUpdate, setLastWebsiteUpdate] = useState(
-		new Date().toDateString()
-	);
-
-	useEffect(() => {
-		setLastWebsiteUpdate(new Date(document.lastModified).toDateString());
-	}, []);
-
-	return (
-		<StyledFooter>
-			KaleCream Limited &copy; {new Date().getFullYear()} . Last Updated:{" "}
-			{lastWebsiteUpdate} .&nbsp;
-			<CustomLink href="/legal/terms"> Terms of Service</CustomLink>
-		</StyledFooter>
-	);
+  return (
+    <StyledFooter>
+      KaleCream Limited &copy; {new Date().getFullYear()}
+      <CustomLink href="/legal/terms"> Terms of Service</CustomLink>
+    </StyledFooter>
+  );
 };
