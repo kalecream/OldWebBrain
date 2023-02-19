c7a371263574a0c6d959db8f0ca994884f896066
kalecream <sabrinamedwinter@gmail.com>
2023-02-18 21:25:15 -0500

    fix: Wrap page layout in body

--- containers/layout/page.tsx
+++ containers/layout/page.tsx
@@ -5,36 +5,36 @@ import TagManager from "react-gtm-module";
 import { MetaProps } from "../../types/layout";
 
 type LayoutProps = {
-	children: React.ReactNode;
-	customMeta?: MetaProps;
-	title?: string;
-	description?: string;
+  children: React.ReactNode;
+  customMeta?: MetaProps;
+  title?: string;
+  description?: string;
 };
 
 export const WEBSITE_HOST_URL = "https://www.kalecream.com";
 
 export const Page = ({
-	children,
-	title,
-	description,
-	customMeta,
+  children,
+  title,
+  description,
+  customMeta,
 }: LayoutProps) => {
-	useEffect(() => {
-		TagManager.initialize({ gtmId: "UA-148483444-1" });
-	}, []);
-	return (
-		<div style={{ height: "95vh" }}>
-			<header>
-				<title>KaleCream {title ? "| " + title : null}</title>
-				<Navigation />
-			</header>
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "UA-148483444-1" });
+  }, []);
+  return (
+    <body style={{ height: "95vh" }}>
+      <header>
+        <title>KaleCream {title ? "| " + title : null}</title>
+        <Navigation />
+      </header>
 
-			<main>
-				<>{children}</>
-			</main>
-			<Footer />
-		</div>
-	);
+      <main>
+        <>{children}</>
+      </main>
+      <Footer />
+    </body>
+  );
 };
 
 export default Page;
