a69d094f9d1af6e5b9085274cf282e5f7fb2449e
kalecream <sabrinamedwinter@gmail.com>
2023-04-24 18:22:46 -0500

    Add twitter

--- components/socialmedia.ts
+++ components/socialmedia.ts
@@ -5,6 +5,11 @@ export const MediaProfiles = [
     color: "#333333",
   },
   {
+    name: "Twitter",
+    url: "https://twitter.com/kalecream",
+    color: "#333333",
+  },
+  {
     name: "Email",
     url: "mailto:mail@kalecream.com",
     color: "#333333",
