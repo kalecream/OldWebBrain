43b7aa1667d777d0d2076843965b453d5e26cf62
kalecream <sabrinamedwinter@gmail.com>
2023-04-24 18:22:14 -0500

    Remove mastodon

--- components/socialmedia.ts
+++ components/socialmedia.ts
@@ -1,17 +1,12 @@
 export const MediaProfiles = [
-    {  
-        name: 'Github',
-        url: 'https://github.com/kalecream',
-        color: '#333333'
-    },
-    {
-        name: 'Mastodon',
-        url: 'https://social.kalecream.com/@sab',
-        color: '#f48024'
-    },
-    {
-        name: 'Email',
-        url: 'mailto:mail@kalecream.com',
-        color: '#333333'
-    }
-]
+  {
+    name: "Github",
+    url: "https://github.com/kalecream",
+    color: "#333333",
+  },
+  {
+    name: "Email",
+    url: "mailto:mail@kalecream.com",
+    color: "#333333",
+  },
+];
