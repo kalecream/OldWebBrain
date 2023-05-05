61b7afe7656024a3a0cdca2f4847596687df7f34
kalecream <sabrinamedwinter@gmail.com>
2023-05-05 09:10:15 -0500

    feat: Theme switch hover effect

--- components/navigation/ThemeSwitch.tsx
+++ components/navigation/ThemeSwitch.tsx
@@ -1,6 +1,9 @@
 import { useTheme } from "next-themes";
 import React from "react";
 import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import styled from "@emotion/styled";
 
 /**
  * Based off of gatsby-theme-novela
@@ -9,36 +12,45 @@ import { useEffect } from "react";
  * https://github.com/ChangoMan/nextjs-typescript-mdx-blog/blob/main/components/ThemeSwitch.tsx
  */
 
+const FAIcon = styled(FontAwesomeIcon)`
+  color: var(--primary);
+
+  &:hover {
+    color: var(--accent);
+    scale: 1.5;
+  }
+`;
+
 const ThemeSwitch = () => {
-	const { theme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
-	useEffect(() => {}, [theme]);
+  useEffect(() => {}, [theme]);
 
-	return (
-		<div
-			style={{
-				display: "flex",
-				placeItems: "center",
-			}}
-		>
-			<button
-				style={{
-					display: "grid",
-					placeItems: "center",
-					borderRadius: "50%",
-					outline: "none",
-					border: "none",
-					fontSize: "1.5rem",
-					padding: "0.5rem",
-					cursor: "pointer",
-					backgroundColor: "transparent",
-				}}
-				onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-			>
-				{theme === "dark" ? "🔅" : "🔆"}
-			</button>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        display: "flex",
+        placeItems: "center",
+      }}
+    >
+      <button
+        style={{
+          display: "grid",
+          placeItems: "center",
+          borderRadius: "50%",
+          outline: "none",
+          border: "none",
+          fontSize: "1.5rem",
+          cursor: "pointer",
+          justifyContent: "center",
+          backgroundColor: "transparent",
+        }}
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        {theme === "dark" ? <FAIcon icon={faSun} /> : <FAIcon icon={faMoon} />}
+      </button>
+    </div>
+  );
 };
 
 export default ThemeSwitch;
