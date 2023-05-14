6d7418c4e982416a29ed1e751483017df7215ead
kalecream <sabrinamedwinter@gmail.com>
2023-05-13 22:50:33 -0500

    Add books page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,22 @@
 import { Section } from "@components/global";
 import Page from "../containers/layout/page";
 import CurrentReads from "@components/about/currentReads";
+import {
+  BacklogGraph,
+  BookShelf,
+  GenreRadarChart,
+} from "@components/about/backlogGraph";
+import Books from "@data/books";
 
 export const About = () => {
   return (
     <Page>
-      <Section style={{ marginTop: "5rem" }}>
+      <Section style={{ marginTop: "5rem", gap: "3rem" }}>
         <h1>About</h1>
         <CurrentReads />
+        <BacklogGraph books={Books} />
+        <GenreRadarChart books={Books} />
+        <BookShelf books={Books} />
       </Section>
     </Page>
   );
