67f63bd0cfee3361addcd9eb773edc1d3d3412dd
kalecream <sabrinamedwinter@gmail.com>
2023-05-30 01:56:07 -0500

    feat: Photo Gallery in About

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,10 +5,12 @@ import CurrentReads from "@components/about/currentReads";
 import { BacklogGraph } from "@components/about/backlogGraph";
 import Books from "@data/books";
 import { use, useEffect, useState } from "react";
+import PhotoGalley from "./gallery";
+
 
 const AboutParagraph = styled.div`
 margin: 0 auto;
-max-width: 40rem;
+max-width: 45rem;
 text-align: justify;
 
 & > h2 {
@@ -17,16 +19,7 @@ text-align: justify;
 }
 `;
 
-const AboutImage = styled.img`
-  float: left;
-  max-width: 300px;
-  margin: auto;
- margin-right: 2rem;
 
-  @media (max-width: 1024px) {
-    display: none;
-  }
-`;
 
 
 export const About = () => {
@@ -81,7 +74,8 @@ export const About = () => {
 
           <p>You see, my story begins in the near ancient times of the internet, crafting Tumblr pages and forum themes. Yep, you heard right! I was chiseling away at code blocks when cat videos were the pinnacle of online entertainment.</p>
 
-           <AboutImage src={"/cats.png"} /> 
+          
+        <PhotoGalley />
 
           <p>I made this about page because showing you my personality through the things I like is personally preferable to describing it. I've always disliked that "tell me a bit about yourself" in personal environments.</p>
           <p>What's my personality like, you ask? Well, imagine if your favorite playlist, Netflix marathon, and book collection had a baby - that's me! But don't take my word for it. Dive into the treasure trove of media below. It's like a mixtape of my soul.
