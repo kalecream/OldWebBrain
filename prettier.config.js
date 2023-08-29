2177a5028b9864ef57b3634bc5641054d4feb34a
kalecream <sabrinamedwinter@gmail.com>
2023-08-29 07:15:51 -0500

    chore: prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	plugins: [],
+	trailingComma: 'es6',
+	singleQuote: true,
+	useTabs: true,
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	fluid: false,
+	tabWidth: 2,
+	printWidth: 120,
+	semi: true
+};
