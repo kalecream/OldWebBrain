f8371162f115fd0342366a05073c9ab308432f74
kalecream <sabrinamedwinter@gmail.com>
2023-09-03 07:06:11 -0500

    chore: update rolodex

--- src/data/rolodex_list.ts
+++ src/data/rolodex_list.ts
@@ -12,6 +12,12 @@ const RolodexLinks = [
 	{
 		name: 'Hourly Rate Calculator',
 		links: 'https://clockify.me/hourly-rate-calculator'
+	},
+	{
+		name: 'https://hackernoon.com/10-money-lessons-i-wish-i-knew-when-i-was-25?source=rss',
+		links: 'https://hackernoon.com/10-money-lessons-i-wish-i-knew-when-i-was-25?source=rss',
+		quote:
+			"Develop multiple income streams: In today{'}s economy, job security is an illusion, and 1 stream of income is 1 away from 0. Those who create significant wealth have multiple streams of income, such as investments in real estate, stocks, or businesses."
 	}
 ];
 
