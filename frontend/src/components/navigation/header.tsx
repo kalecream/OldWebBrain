512bccd549c996c2c51b5c475fe13cf0c0516fdd
kalecream <sabrinamedwinter@gmail.com>
2023-09-09 12:59:57 -0500

    refactor: update navbar component

--- frontend/src/components/navigation/header.tsx
+++ frontend/src/components/navigation/header.tsx
@@ -1,27 +1,10 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import Directory from '../../data/directory';
+import Directory from '@data/directory';
 import ThemeSwitch from './theme-toggle';
 import { useRouter } from 'next/router';
-// import AvailableForWork from "@components/navigation/looking-work";
+import AvailableForWork from "@components/navigation/looking-work";
 import Link from 'next/link';
 import style from './header.module.scss';
-
-import { FaBars } from 'react-icons/fa6';
-
-const Hamburger = styled(FaBars)`
-	display: none;
-	color: var(--muted);
-	@media screen and (max-width: 768px) {
-		display: block;
-		font-size: 1.5rem;
-		top: 0;
-		right: 0;
-		position: absolute;
-		cursor: pointer;
-		transform: translate(-100%, 75%);
-	}
-`;
+// import { FaBars } from 'react-icons/fa6';
 
 export const Header: React.FunctionComponent = () => {
 	const router = useRouter();
@@ -30,7 +13,7 @@ export const Header: React.FunctionComponent = () => {
 		<header>
 			{router?.pathname !== '/' && (
 					<Link className={style['site-name']} href="/">
-						Sabrina
+						sabrina
 					</Link>
 				)}
 			<div className={`${style['directory-list']} ${style['directory-section']}`}>
@@ -46,7 +29,7 @@ export const Header: React.FunctionComponent = () => {
 			</div>
 			<div className={`${style.settings}  ${style['directory-section']}`}>
 				<ThemeSwitch />
-				{/* <AvailableForWork /> */}
+				<AvailableForWork />
 			</div>
 		</header>
 	);
