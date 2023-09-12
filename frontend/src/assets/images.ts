932e284e0e898c537efb56f5d36dce6f7c17cfdf
kalecream <sabrinamedwinter@gmail.com>
2023-09-12 17:26:23 -0500

    chore: remove unused import

--- frontend/src/assets/images.ts
+++ frontend/src/assets/images.ts
@@ -1,6 +1,4 @@
-import { Caption } from '../components/_basics/Basics';
 import img_20220102 from '../assets/images/3D/2023/plant_20230102_composed.png';
-
 import Chess from '../assets/images/3D/chess__alternate_view__by_kalecream.jpg';
 import Park from '../assets/images/3D/park_by_kalecream.jpg';
 import UnderConstruction from '../assets/images/Under_Construction_by_kalecream.webp';
