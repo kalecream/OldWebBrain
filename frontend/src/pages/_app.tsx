07c3e55c84f136995aa9ebdfe93de8d3fb7df562
kalecream <sabrinamedwinter@gmail.com>
2023-09-12 19:58:28 -0500

    fix: add metadata
    It seems that metadata only works in the _app???
    
    https://stackoverflow.com/questions/71305450/og-image-meta-tag-with-next-js-is-not-working

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,10 +3,12 @@ import { ThemeProvider } from 'next-themes';
 
 import type { AppProps } from 'next/app';
 import { MetaProps } from '../types/layout';
+import Head from 'next/head';
 
 export const WEBSITE_HOST_URL = 'https://www.sabrinamedwinter.com';
 
 export const App = ({ Component, pageProps }: AppProps, { customMeta }: { customMeta?: MetaProps }): JSX.Element => {
+
 	const meta: MetaProps = {
 		title: 'SM',
 		description:
@@ -16,7 +18,12 @@ export const App = ({ Component, pageProps }: AppProps, { customMeta }: { custom
 		...customMeta
 	};
 
+	const defaultTitle = 'Sabrina Medwinter';
+const defaultDescription =
+	'Unlocking the digital realm with a fusion of Jamaican web development prowess and captivating 3D artistry.';
+
 	return (
+		
 		<ThemeProvider
 			storageKey="theme"
 			attribute="class"
@@ -25,6 +32,53 @@ export const App = ({ Component, pageProps }: AppProps, { customMeta }: { custom
 			defaultTheme="system"
 			themes={['light', 'dark']}
 		>
+			<Head>
+				{/* Recommended Meta Tags */}
+				<meta charSet="utf-8" />
+				<meta name="language" content="english" />
+				<meta httpEquiv="content-type" content="text/html" />
+				<meta name="author" content={  'Sabrina Medwinter'} />
+
+				{/* Search Engine Optimization Meta Tags */}
+				<title>{meta.title ?? defaultTitle}</title>
+				<meta name="description" content={meta.description ?? defaultDescription} />
+				<meta
+					name="keywords"
+					content="Software Engineer, Web Developer, Frontend Developer, Creative Developer, React Developer, Filipino Developer, 3D Develper"
+				/>
+				<meta name="robots" content="index,follow" />
+				<meta name="distribution" content="web" />
+				{/* 
+      Facebook Open Graph meta tags
+        documentation: https://developers.facebook.com/docs/sharing/opengraph */}
+				<meta name="og:title" content={meta.title} />
+				<meta name="og:type" content="site" />
+				<meta name="og:url" content={WEBSITE_HOST_URL} />
+				<meta name="og:image" content={'@assets/opengraph/thumbnail.png'} />
+				<meta name="og:site_name" content="Sabrina Medwinter" />
+				<meta name="og:description" content={meta.description} />
+				{/* <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+				<link rel="apple-touch-icon" sizes="16x16" href="/icons/favicon-16x16.png" />
+				<link rel="apple-touch-icon" sizes="32x32" href="/icons/favicon-32x32.png" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" /> */}
+				<link rel="manifest" href="/manifest.json" />
+				<link rel="mask-icon" color="#000000" href="/icons/safari-pinned-tab.svg" />
+				<link rel="apple-touch-startup-image" href="@assets/opengraph/thumbnail.png" />
+
+				{/* Meta Tags for HTML pages on Mobile */}
+				{/* <meta name="format-detection" content="telephone=yes"/>
+        <meta name="HandheldFriendly" content="true"/>  */}
+				<meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1.0" />
+				<meta name="theme-color" content="#000" />
+				<link rel="shortcut icon" href="@assets/opengraph/thumbnail.png" />
+
+				{/* 
+      Twitter Summary card
+        documentation: https://dev.twitter.com/cards/getting-started
+        Be sure validate your Twitter card markup on the documentation site. */}
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@SabMedwinter" />
+			</Head>
 			<Component {...pageProps} />
 		</ThemeProvider>
 	);
