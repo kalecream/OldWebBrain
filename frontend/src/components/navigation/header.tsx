17db05eca1d4866cc46eba79be6afb7d575d93a9
kalecream <sabrinamedwinter@gmail.com>
2023-09-14 06:11:13 -0500

    chore: remove refs to removed components

--- frontend/src/components/navigation/header.tsx
+++ frontend/src/components/navigation/header.tsx
@@ -1,10 +1,7 @@
 import Directory from '@data/directory';
 import ThemeSwitch from './theme-toggle';
-import { useRouter } from 'next/router';
-import AvailableForWork from "@components/navigation/AvailableForWork";
-import Link from 'next/link';
+import { useRouter } from 'next/router';import Link from 'next/link';
 import style from './header.module.scss';
-// import { FaBars } from 'react-icons/fa6';
 
 export const Header: React.FunctionComponent = () => {
 	const router = useRouter();
@@ -16,7 +13,6 @@ export const Header: React.FunctionComponent = () => {
 					<Link className={style['site-name']} href="/">
 						sabrina
 					</Link>
-					{/* <AvailableForWork /> */}
 					</>
 				)}
 			<div className={`${style['directory-list']} ${style['directory-section']}`}>
@@ -32,7 +28,6 @@ export const Header: React.FunctionComponent = () => {
 			</div>
 			<div className={`${style.settings}  ${style['directory-section']}`}>
 				<ThemeSwitch />
-				
 			</div>
 		</header>
 	);
