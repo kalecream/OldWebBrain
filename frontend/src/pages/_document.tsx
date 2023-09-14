d23e563f43dab40d1853d080732856ef1f928a82
kalecream <sabrinamedwinter@gmail.com>
2023-09-13 19:28:47 -0500

    chore: erase imports
    build: un-customise document for next
    
    a dumb mistake. Head in next/document is not the same in next/head

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Document, { DocumentContext } from 'next/document';
-import { WEBSITE_HOST_URL } from './_app';
 
 const Document = () =>{
 		return (
