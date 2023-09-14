805811330724ba02f2a2b3d1a25202aa82376749
kalecream <sabrinamedwinter@gmail.com>
2023-09-14 06:09:05 -0500

    refactor: remove blog from front-page
    until sanity implementation

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,32 +1,18 @@
-import { GetStaticProps } from 'next';
-import { getAllPosts } from '@utils/api';
-import { PostType } from './blog';
-
 import Page from '@containers/layout/page';
-
 import Hero from '@components/hero/hero';
 import { SiteBackground } from '@components/threeJS/scene';
 import ProjectList from '@components/projects/projectsList';
-import BlogList from './blog';
 
-export const Home = ({ posts }: PostType ): JSX.Element => {
+export const Home = (): JSX.Element => {
 	return (
 		<Page>
 			<SiteBackground />
 			<Hero />
-			<BlogList posts={posts}  />
+			{/* TODO: move blog posts to Sanity} */}
+			{/* <BlogList posts={posts}  /> */}
 			<ProjectList />
 		</Page>
 	);
 };
-
-export const getStaticProps: GetStaticProps = async () => {
-	const posts = getAllPosts(['date', 'description', 'slug', 'title', 'coverImage', 'tags']);
-
-	return {
-		props: { posts }
-	};
-};
-
 
 export default Home;
