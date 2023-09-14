e3c6ac437512a9c78e70b2a4a0159565415a43d1
kalecream <sabrinamedwinter@gmail.com>
2023-09-13 19:59:00 -0500

    refactor: error page

--- frontend/src/pages/_error.tsx
+++ frontend/src/pages/_error.tsx
@@ -1,109 +1,29 @@
-import styled from '@emotion/styled';
-import Page from '../containers/layout/page';
-
-const Background = styled.div`
-	background-size: cover;
-	background-position: center;
-	background-repeat: no-repeat;
-	width: 100%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	@media (max-width: 1100px) {
-		flex-wrap: wrap;
-	}
-`;
-// TODO: Fix colors
-const Text = styled.h1`
-	@media (max-width: 400px) {
-		font-size: 5rem;
-	}
-	position: relative;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	height: 100%;
-	font-size: 20rem;
-	font-weight: 900;
-	letter-spacing: 1rem;
-	text-align: center;
-	color: transparent;
-	stroke: 1px var(--primary);
-	-webkit-text-stroke: 1px grey;
-	-webkit-text-fill-color: transparent;
-	background: repeating-linear-gradient(45deg, darkgreen 25%, lightgreen 50%, darkgreen 50%, lightgreen 75%);
-	background-size: 100px 100px;
-	background-clip: text;
-	-webkit-background-clip: text;
-	animation: stripeBackgroundPosition 2s linear infinite;
-
-	@keyframes stripeBackgroundPosition {
-		0% {
-			background-position: 0 0;
-		}
-		100% {
-			background-position: calc(100px * -1) 0;
-		}
-	}
-	@keyframes stripeBackgroundPosition {
-		0% {
-			background-position: 0 0;
-		}
-		100% {
-			background-position: calc(100px * -1) 0;
-		}
-	}
-
-	@keyframes stripeTransform {
-		0% {
-			transform: translateX(0);
-		}
-		100% {
-			transform: translateX(calc(100px * -1));
-		}
-	}
-`;
-
-const TextContainer = styled.div`
-	width: 80%;
-	display: flex;
-	flex-direction: column;
-	place-items: center;
-	text-align: center;
-	@media (max-width: 750px) {
-		width: 100%;
-	}
-`;
-
-const TextDescription = styled.p`
-	margin-top: 0.2rem;
-	color: grey;
-`;
-
-const TextDescriptionContainer = styled.div`
-	width: 50%;
-	display: flex;
-	flex-direction: column;
-	place-items: center;
-	text-align: center;
-	@media (max-width: 750px) {
-		width: 100%;
-	}
-`;
+import Page from '@containers/layout/page';
+import styles from '@styles/modules/Errors.module.scss';
+import Link from 'next/link';
 
 export const Error = ({ statusCode }) => {
 	return (
 		<Page>
-			<Background>
-				<TextContainer>
-					<Text>{statusCode ? `An error ${statusCode} occurred on server` : 'An error occurred on client'}</Text>
-					<TextDescriptionContainer>
-						<TextDescription>The page you are looking for does not exist.</TextDescription>
-						<TextDescription>Please check the URL and try again.</TextDescription>
-					</TextDescriptionContainer>
-				</TextContainer>
-			</Background>
+			<section className={styles.container}>
+				<div className={styles.error__container}>
+					<h1 className={styles.error__code}>{statusCode}</h1>
+					<div className={styles.error__description__container}>
+						<h2 className={styles.error_description}>
+							{statusCode ? (
+								
+							statusCode === 404 ? (
+								`This page could not be found`
+								) : `An error occurred on server`
+							)
+							 :
+							'An error occurred on client'
+						}
+						</h2 >
+							<Link href="/">Back to Home</Link>
+					</div>
+				</div>
+			</section>
 		</Page>
 	);
 };
