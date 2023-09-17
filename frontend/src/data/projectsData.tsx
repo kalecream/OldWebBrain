56e5bf5fd2940b8af1123d3ae0aa11627909ca8a
kalecream <sabrinamedwinter@gmail.com>
2023-09-16 22:53:46 -0500

    chore: update projectsData

--- frontend/src/data/projectsData.tsx
+++ frontend/src/data/projectsData.tsx
@@ -30,8 +30,8 @@ export const Projects: ProjectStructure[] = [
 		description: (
 			<span>
 				This is a duotone i3wm theme. I installed i3wm because for a while, my mouse wasn't working properly, so I had
-				to use the keyboard to navigate. Nowadays, I like it because it's fast and minimal. I don't have to size the
-				windows myself.
+				to use the keyboard to navigate. Nowadays, I like it because it's fast, minimal and my mice still keep dying on
+				me.
 			</span>
 		),
 		repoName: 'dotfiles',
@@ -50,11 +50,11 @@ export const Projects: ProjectStructure[] = [
 			</a>,
 			<a key="pb" href="https://github.com/polybar/polybar">
 				Polybar
-			</a>
+			</a>,
 		],
 		language: ['Bash', 'Python'],
 		created: '2022-07-07',
-		updated: '2023-08-13'
+		updated: '2023-08-13',
 	},
 	{
 		id: '002',
@@ -62,6 +62,7 @@ export const Projects: ProjectStructure[] = [
 		title: 'Morning Messages',
 		category: 'code',
 		type: 'automation',
+		status: 'completed',
 		description: (
 			<span>
 				A simple python script to automate the process of sending current news articles and cute kaomoji-filled messages
@@ -78,10 +79,10 @@ export const Projects: ProjectStructure[] = [
 			</a>,
 			<a key="owm" href="https://openweathermap.org/">
 				OpenWeatherMap API{' '}
-			</a>
+			</a>,
 		],
 		language: ['Python'],
-		created: '2021-11-15'
+		created: '2021-11-15',
 	},
 	{
 		id: '003',
@@ -90,8 +91,12 @@ export const Projects: ProjectStructure[] = [
 		category: 'code',
 		type: 'website',
 		image: '/img/project/003.webp',
+		status: 'ongoing',
 		description: (
-			<span>I've rebuilt this website many different times in plain Html/Css/Js, Wordpress and now in React Typescript. You're on it right now.</span>
+			<span>
+				I've rebuilt the website many different times in; plain HTML, Javascript and CSS combination, Wordpress, and now
+				in React Typescript on NextJS. You're on it right now.
+			</span>
 		),
 		repoName: 'OldWebBrain',
 		link: 'https://www.sabrinamedwinter.com/',
@@ -104,10 +109,16 @@ export const Projects: ProjectStructure[] = [
 			</a>,
 			<a key="0033" href="https://greensock.com/gsap/">
 				GSAP{' '}
-			</a>
+			</a>,
+			<a key="0034" href={'https://sanity.io/'}>
+				Sanity
+			</a>,
+			<a key="0035" href={'https://prisma.io'}>
+				Prisma
+			</a>,
 		],
 		created: '2022-08',
-		language: ['React', 'TypeScript', 'MDX']
+		language: ['React', 'TypeScript', 'MDX', 'ProsgreSQL'],
 	},
 	{
 		id: '004',
@@ -115,37 +126,40 @@ export const Projects: ProjectStructure[] = [
 		title: 'GPT-4 Record',
 		category: 'code',
 		type: ['automation', 'AI'],
+		status: 'archived',
 		description: (
 			<span>
 				A one-hour paid project to accept a 800 word prompt, perform a specfic action with the prompt and use that
-				output as a second prompt. Both prompts were then saved to an ordinary text file. The source is not available because it was released by the client.
+				output as a second prompt. Both prompts were then saved to an ordinary text file. The source is not available
+				because it was released by the client.
 			</span>
 		),
 		technology: [
 			<a key="00401" href="https://openai.com/blog/openai-api/">
 				OpenAI API{' '}
-			</a>
+			</a>,
 		],
 		language: ['Python'],
-		created: '2023-05-30'
+		created: '2023-05-30',
 	},
 	{
 		id: '005',
 		display: true,
 		title: 'Swagger Login Test',
 		category: 'code',
+		status: 'completed',
 		type: 'test',
 		link: 'https://sphenery.vercel.app/login',
 		image: '/img/project/005.webp',
-		description: <span>A form done for a test of the Swagger API.</span>,
+		description: <span>A form done for a test of a Swagger API.</span>,
 		technology: [
 			<a key="00501" href="https://sphenery.com/swagger/index.html">
 				Swagger SMTest{' '}
-			</a>
+			</a>,
 		],
 		repoName: 'Sphenery',
 		language: ['React', 'Typescript'],
-		created: '2023-07-23'
+		created: '2023-07-23',
 	},
 	{
 		id: '006',
@@ -154,10 +168,13 @@ export const Projects: ProjectStructure[] = [
 		category: 'illustration',
 		type: 'practice',
 		image: '/img/project/006.webp',
-		description: <span>A test of the sapling plug-in in Blender3D. This model is set as the background of this website.</span>,
+		status: 'completed',
+		description: (
+			<span>A test of the sapling plug-in in Blender3D. This model is set as the background of this website.</span>
+		),
 		technology: [<a key="00601">Blender3D</a>],
 		language: [],
-		created: '2019-01-01'
+		created: '2019-01-01',
 	},
 	{
 		id: '007',
@@ -166,10 +183,16 @@ export const Projects: ProjectStructure[] = [
 		category: 'illustration',
 		type: 'practice',
 		image: '/img/project/007.webp',
-		description: <span>A 3D logo I made for a podcast idea to talk about the media I consume each week. The podcast has been put on hiatus for now.</span>,
+		status: 'completed',
+		description: (
+			<span>
+				A 3D logo I made for a podcast idea to talk about the media I consume each week. The podcast has been put on
+				hiatus for now.
+			</span>
+		),
 		technology: [<a key="00701">Blender3D</a>],
 		language: [],
-		created: '2023-01-01'
+		created: '2023-01-01',
 	},
 	{
 		id: '008',
@@ -178,10 +201,15 @@ export const Projects: ProjectStructure[] = [
 		category: 'illustration',
 		type: 'production',
 		image: '/img/project/008.webp',
-		description: <span>A placeholder image I made for when my website was  under construction. It still is Under Construction.</span>,
+		status: 'completed',
+		description: (
+			<span>
+				A placeholder image I made for when my website was under construction. It still is Under Construction.
+			</span>
+		),
 		technology: [<a key="00801">Blender3D</a>],
 		language: [],
-		created: '2022-11-03'
+		created: '2022-11-03',
 	},
 	{
 		id: '009',
@@ -190,9 +218,11 @@ export const Projects: ProjectStructure[] = [
 		category: 'illustration',
 		type: 'practice',
 		image: '/img/project/009.webp',
-		description: <span>A 3D render of a strawberry milkshake I made.</span>,
+		status: 'completed',
+		description: <span>A 3D render of a strawberry milkshake I made after for a dailySketch</span>,
 		technology: ['Blender3D'],
-		created: '2022-11-12'
+		language: [],
+		created: '2022-11-12',
 	},
 	{
 		id: '010',
@@ -201,9 +231,16 @@ export const Projects: ProjectStructure[] = [
 		category: 'illustration',
 		type: 'plan',
 		image: '/img/project/010.webp',
-		description: <span>A mockup of a concrete kiln I planned to make. This illustration is to scale.</span>,
-		technology: ['Blender'],
-		created: '2022-11-12'
+		status: 'completed',
+		description: (
+			<span>
+				A mockup of a concrete kiln I planned to make to experiment with making pottery at home. This illustration is to
+				scale.
+			</span>
+		),
+		language: [],
+		technology: [<a key="01001">Blender3D</a>],
+		created: '2022-11-12',
 	},
 	{
 		id: '011',
@@ -212,9 +249,13 @@ export const Projects: ProjectStructure[] = [
 		category: 'illustration',
 		type: 'art-fight',
 		image: '/img/project/011.webp',
-		description: <span>This render was for a competition on Twitter to create a brand for baby toys.</span>,
-		technology: ['Blender'],
-		created: '2022-11-12'
+		status: 'completed',
+		description: (
+			<span>This render was done as a late entry for a competition on Twitter to create a brand for baby toys.</span>
+		),
+		technology: [<a key="01101">Blender3D</a>],
+		language: [],
+		created: '2022-11-12',
 	},
 	{
 		id: '012',
@@ -223,9 +264,11 @@ export const Projects: ProjectStructure[] = [
 		category: 'illustration',
 		type: 'practice',
 		image: '/img/project/012.webp',
+		status: 'completed',
 		description: <span>A practice render I made for the idea of a new queen coming onto a chess board.</span>,
-		technology: ['Blender'],
-		created: '2022-11-12'
+		technology: [<a key="01101">Blender3D</a>],
+		language: [],
+		created: '2022-11-12',
 	},
 	{
 		id: '013',
@@ -234,21 +277,31 @@ export const Projects: ProjectStructure[] = [
 		category: 'illustration',
 		type: 'practice',
 		image: '/img/project/013.webp',
-		description: <span>A practice render I made because I wanted to make more little cakes.</span>,
-		technology: ['Blender'],
-		created: '2022-11-12'
+		status: 'ongoing',
+		description: (
+			<span>
+				I wanted to make more little cakes. I made one cake as an icon and never made any more cakes. I should make some
+				more cakes.
+			</span>
+		),
+		technology: [<a key="01301">Blender3D</a>],
+		language: [],
+		created: '2022-11-12',
 	},
 	{
 		id: '014',
 		display: true,
-		title: 'Cake',
+		title: 'Donut',
 		category: 'illustration',
 		type: 'practice',
 		image: '/img/project/014.webp',
-		description: <span>A practice render I made because I wanted to make more little cakes.</span>,
-		technology: ['Blender'],
-		created: '2022-11-12'
-	}
+		status: 'completed',
+		description: (
+			<span>I wanted to do the infamous beginner donut again, but ended up doing a breakfast scene again.</span>
+		),
+		technology: [<a key="01401">Blender3D</a>],
+		created: '2022-11-12',
+	},
 ];
 
 export default Projects;
