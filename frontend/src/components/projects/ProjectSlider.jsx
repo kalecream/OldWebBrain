d09e705b4cb9b5828642e9d861df76cd3dd5fa63
kalecream <sabrinamedwinter@gmail.com>
2023-09-16 22:58:00 -0500

    refactor: remove indexBkg and projectsSlider

--- frontend/src/components/projects/ProjectSlider.jsx
+++ frontend/src/components/projects/ProjectSlider.jsx
@@ -1,8 +1,10 @@
 import { useRef } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import ProjectsData from '@data/projectsData';
 import styles from '@styles/modules/ProjectSlider.module.scss';
 import * as PIXI from 'pixi.js';
+import { GetMonthName } from '@utils/GetMonthName';
 
 export const ProjectSlider = () => {
     const loadRef = useRef(null);
@@ -54,72 +56,108 @@ export const ProjectSlider = () => {
     window.onload = () => {
         document.getElementById('loadMore').onmousedown = dragStart;
         document.getElementById('loadMore').onmouseup = stopDrag;
-    }
-
-    const app = new PIXI.Application();
-    document.body.appendChild(app.view);
-
-    app.stage.interactive = true;
-
-    const container = new PIXI.Container();
-    app.stage.addChild(container);
-
-    const flag = PIXI.Sprite.from('examples/assests/pixi-filters/flag.png');
-    container.addChild(flag);
-    flag.x = 100;
-    flag.y = 100;
-
-    const displacementSprite = PIXI.Sprite.from('examples/assests/pixi-filters/displacement_map_repeat.jpg');
-    displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-    const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
-    displacementFilter.padding = 10;
-
-    app.stage.addChild(displacementSprite);
-    flag.filters = [displacementFilter];
-
-    displacementFilter.scale.x = 30;
-    displacementFilter.scale.y = 60;
-
-    app.ticker.add(() => {
-        //  offset the spite position to make vFilterCoord update to larger value Repeat wrapping makes sure there's still pixels on the coordinates
-        displacementSprite.x += 1;
-        // reset x to 0 when it's over width to keep values from going to very huge numbers.
-        if (displacementSprite.x > displacementSprite.width) {
-            displacementSprite.x = 0;
-        }
-    });
-
-    const thumbnail__image = loadRef.current.getElementsByClassName('drag__item__image');
-
-    thumbnail__image.addEventListener('mouseover', () => {
-        thumbnail__image.style.filter = 'blur(0px)';
-    })
-
-    const playgrounds = [];
-
-    let displacesments = () => {
-        for (let i = 0; i < 10; i++) {
-            let playground = {
-                id: i,
-                displacement: PIXI.Sprite.from('examples/assests/pixi-filters/displacement_map_repeat.jpg'),
-                displacementFilter: new PIXI.filters.DisplacementFilter(playground.displacement),
-                displacementSprite: PIXI.Sprite.from('examples/assests/pixi-filters/displacement_map_repeat.jpg'),
-            }
-            playgrounds.push(playground);
+        if (loadRef.current) {
+            displacement();
         }
     }
 
-    Array.from(thumbnail__image).map((item, index) => {
-        let playground = {};
-        //  set up render
-        playground.renderer = PIXI.autoDetectRenderer({ width: thumbnail__image[index].offsetWidth, height: thumbnail__image[index].offsetHeight, transparent: true });
-        playground.renderer.autoResize = true;
-        item.appendChild(playground.renderer.view);
-        let tp = PIXI.Texture.from(list[index].thumb);
-    })
+    // const app = new PIXI.Application();
+    // document.body.appendChild(app.view);
 
-    //  https://medium.com/fink-it/draggable-carousel-with-webgl-effects-a69b20780f27
-    //  TODO: FINISH this
+    // app.stage.interactive = true;
+
+    // const container = new PIXI.Container();
+    // app.stage.addChild(container);
+
+    // const flag = PIXI.Sprite.from('examples/assests/pixi-filters/flag.png');
+    // container.addChild(flag);
+    // flag.x = 100;
+    // flag.y = 100;
+
+    // const displacementSprite = PIXI.Sprite.from('examples/assests/pixi-filters/displacement_map_repeat.jpg');
+    // displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+    // const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+    // displacementFilter.padding = 10;
+
+    // app.stage.addChild(displacementSprite);
+    // flag.filters = [displacementFilter];
+
+    // displacementFilter.scale.x = 30;
+    // displacementFilter.scale.y = 60;
+
+    // app.ticker.add(() => {
+    //     //  offset the spite position to make vFilterCoord update to larger value Repeat wrapping makes sure there's still pixels on the coordinates
+    //     displacementSprite.x += 1;
+    //     // reset x to 0 when it's over width to keep values from going to very huge numbers.
+    //     if (displacementSprite.x > displacementSprite.width) {
+    //         displacementSprite.x = 0;
+    //     }
+    // });
+
+    // const thumbnail__image = loadRef?.current.getElementsByClassName('drag__item__image');
+
+    // thumbnail__image.addEventListener('mouseover', () => {
+    //     thumbnail__image.style.filter = 'blur(0px)';
+    // })
+
+    // const playgrounds = [];
+
+    // let displacesments = () => {
+    //     for (let i = 0; i < 10; i++) {
+    //         let playground = {
+    //             id: i,
+    //             displacement: PIXI.Sprite.from('examples/assests/pixi-filters/displacement_map_repeat.jpg'),
+    //             displacementFilter: new PIXI.filters.DisplacementFilter(playground.displacement),
+    //             displacementSprite: PIXI.Sprite.from('examples/assests/pixi-filters/displacement_map_repeat.jpg'),
+    //         }
+    //         playgrounds.push(playground);
+    //         animate();
+    //     }
+    // }
+
+    // Array.from(thumbnail__image).map((item, index) => {
+    //     let playground = {};
+    //     //  set up render
+    //     playground.renderer = PIXI.autoDetectRenderer({ width: thumbnail__image[index].offsetWidth, height: thumbnail__image[index].offsetHeight, transparent: true });
+    //     playground.renderer.autoResize = true;
+    //     item.appendChild(playground.renderer.view);
+    //     let tp = PIXI.Texture.from(list[index].thumb);
+    // })
+
+    // //  https://medium.com/fink-it/draggable-carousel-with-webgl-effects-a69b20780f27
+    // //  TODO: FINISH this
+    // let preview = new PIXI.sprite(tp);
+    // preview.anchor.set(0.5);
+    // preview.width = playground.renderer.width;
+    // preview.height = playground.renderer.height;
+    // preview.x = playground.renderer.width / 2;
+    // preview.y = playground.renderer.height / 2;
+    // playground.preview = preview;
+
+    // // let displacementSprite = PIXI.Sprite.from('examples/assests/pixi-filters/displacement_map_repeat.jpg');
+    // // let displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+    // displacementSprite.scale.y = 0;
+    // displacementSprite.scale.x = 0;
+    // displacementSprite.rotation = 90;
+    // let stage = new PIXI.Container();
+    // stage.addChild(displacementSprite);
+
+    // stage.addChild(preview);
+    // playground.stage = stage;
+
+    // playgrounds.push(playground);
+
+    // let animate = () => {
+    //     requestAnimationFrame(animate);
+    //     playgrounds.map(item => {
+    //         item.displacementSprite.scale.x += 0.3;
+    //         item.displacementSprite.scale.y += 0.3;
+    //         item.stage.filters = [item.displacementFilter]
+    //         item.renderer.render(item.stage);
+    //     })
+    // }
+
+
 
     return (
         <div id='loadMore' ref={loadRef}>
@@ -132,8 +170,23 @@ export const ProjectSlider = () => {
                             href={`/project/${project.id}`
                             }>
                             <div className={styles.drag__item}>
+                                <h2 className={styles.drag__item__title}>
+                                    <div className={styles.drag__item__year}>
+                                        {GetMonthName(project.created)}
+                                        {project.created.split('-', 1)}
+                                    </div>
+                                    {project.title}
+                                </h2>
                                 <div className={styles.drag__item__image}>
-                                    <span>{project.type}</span>
+                                    <Image
+                                        loader={({ src, width }) => `${src}?w=${width}`}
+                                        height={0}
+                                        width={0}
+                                        sizes="100vw"
+                                        style={{ width: 'auto', height: '250px', margin: '0 auto', display: 'flex' }}
+                                        src={project.image}
+                                        alt={project.title}
+                                    />
                                 </div>
                                 <div className={styles.drag__item__details}>
                                     <p className={styles.drag__item__title}>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,18 +1,18 @@
 import Page from '@containers/layout/page';
 import Hero from '@components/hero/hero';
-import { SiteBackground } from '@components/threeJS/scene';
+// import { SiteBackground } from '@components/threeJS/scene';
 import ProjectList from '@components/projects/projectsList';
 import { ProjectSlider } from '@components/projects/ProjectSlider';
 
 export const Home = (): JSX.Element => {
 	return (
 		<Page>
-			<SiteBackground />
+			{/* <SiteBackground /> */}
 			<Hero />
 			{/* TODO: move blog posts to Sanity} */}
-			{/* <BlogList posts={posts}  /> */}
+			{/* <BlogList posts={posts} /> */}
 			<ProjectList />
-			<ProjectSlider />
+			{/* <ProjectSlider /> */}
 		</Page>
 	);
 };
