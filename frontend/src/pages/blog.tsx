6fac87c6c66d8793a7c39e56090004c3d90d930f
kalecream <sabrinamedwinter@gmail.com>
2023-10-08 20:05:17 -0500

    fix: re-add Blog component to index

--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 // import { ImageLoader } from '@utils/ImageLoader';
 // import { format, parseISO } from 'date-fns';
 import styles from '@components/blog/articles.module.scss';
+import { format, parseISO } from 'date-fns';
 
 export type PostType = {
 	[x: string]: any;
@@ -21,14 +22,14 @@ export const BlogPage = ({ posts }: PostType): JSX.Element => {
 	return (
 		<Page>
 			<section>
-				<h2 className="section-title">Blog</h2>
+				<h2 className='section-title'>Blog</h2>
 				<div className={ styles.container}>
 					{posts.map((post) => {
 						return (
 							<Link className={styles.article__featured} href={post.slug} >
 								<img src={post.coverImage} />
-								<div className="former-featured-article-content">
-									<p className="post-date">{post.date}</p>
+								<div className='former-featured-article-content'>
+									<p className='post-date'>{post.date}</p>
 									<h2>{post.title}</h2>
 									<p>{post.description}</p>
 								</div>
@@ -45,46 +46,44 @@ export const BlogList = ({ posts }: PostType ): JSX.Element => {
 	return (
 		<>
 			{posts.length > 0 && (
-				<section id="blog" className="fadeIn--below index-section">
-					<h2 className="section-title">Blog</h2>
-					<div className="pancake section-content">
+				<section id='blog' className='fadeIn__below'>
+					<h2 className='section-title'>Blog</h2>
+					<div className='pancake section-content'>
 						{posts.slice(0, 8).map((post) => (
-							<div key={post.slug} className="blog__article pancake-child">
-								{post.coverImage && (
-									<div className="image-wrapper">
+							<Link as={`/posts/${post.slug}`} key={post.slug} href={`/posts/[slug]`} className={`${styles.article} pancake-child`}>
+								{/* {post.coverImage && (
+									<div className={'image__wrapper'}>
 										<Image
 											height={0}
 											width={0}
 											loader={({ src }) => src}
-											sizes="100vw"
+											sizes='100vw'
 											style={{ width: 'auto', height: '100px' }}
 											src={post.coverImage}
 											alt={post.alt ? post.alt : ''}
-											className="blog--article__image"
+											className={styles.article__image}
 										/>
 									</div>
-								)}
-								<div className="blog--article__section">
-									{/* {post.date && (
-										<span className="blog--article__date">{format(parseISO(post.date), 'MMMM dd, yyyy')}</span>
-									)} */}
-									<Link as={`/posts/${post.slug}`} key={post.slug} href={`/posts/[slug]`}>
-										<h2 className="article__title">{post.title}</h2>
-									</Link>
+								)} */}
+								<div className={styles.article__section}>
+									{post.date && (
+										<span className={styles.article__date}>{format(parseISO(post.date), 'MMMM dd, yyyy')}</span>
+									)}
+									<h2 className={styles.article__title}>{post.title}</h2>
 
-									<p className="article__description">{post.description}</p>
+									<p className={styles.article__description}>{post.description}</p>
 
 									{post.tags && (
-										<div className="article__tags">
+										<div className={styles.article__tags}>
 											{post.tags.slice(0, 2).map((tag) => (
-												<Link className="tag" key={tag} href={'/tags/' + tag.replace(/\s+/g, '+')}>
+												<Link className={styles.article__tag} key={tag} href={'/tags/' + tag.replace(/\s+/g, '+')}>
 													{tag}
 												</Link>
 											))}
 										</div>
 									)}
 								</div>
-							</div>
+							</Link>
 						))}
 					</div>
 					{/* TODO: Add weeklogs and tutorials */}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,19 +2,31 @@ import Page from '@containers/layout/page';
 import Hero from '@components/hero/hero';
 // import { SiteBackground } from '@components/threeJS/scene';
 import ProjectList from '@components/projects/projectsList';
+import { BlogList, PostType } from './blog';
+import { getAllPosts } from '@utils/api';
+import { GetStaticProps } from 'next/types';
 // import { ProjectSlider } from '@components/projects/ProjectSlider';
 
-export const Home = (): JSX.Element => {
+export const Home = ({posts}: PostType): JSX.Element => {
 	return (
 		<Page>
 			{/* <SiteBackground /> */}
 			<Hero />
 			{/* TODO: move blog posts to Sanity} */}
-			{/* <BlogList posts={posts} /> */}
+			<BlogList posts={posts} />
 			<ProjectList />
 			{/* <ProjectSlider /> */}
 		</Page>
 	);
 };
+
+export const getStaticProps: GetStaticProps = async () => {
+	const posts = getAllPosts(['date', 'description', 'slug', 'title', 'coverImage', 'tags']);
+
+	return {
+		props: { posts }
+	};
+};
+
 
 export default Home;
