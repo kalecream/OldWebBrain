4b98deafd5ab9bf63c74a59095899c420cdbbb0b
kalecream <sabrinamedwinter@gmail.com>
2023-10-09 08:53:38 -0500

    WIP: add Services Page

---
+++ frontend/src/pages/services.tsx
@@ -0,0 +1,12 @@
+import { ComingSoon } from "@components/_misc/ComingSoon";
+import { Page } from "@containers/layout";
+
+export const Service = () => {
+    return (
+        <Page>
+            <ComingSoon/>
+        </Page>
+    )
+}
+
+export default Service
