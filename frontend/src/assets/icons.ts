e42bb2108a4b2df6b505ffb2a600c2889a4fd160
kalecream <sabrinamedwinter@gmail.com>
2024-01-19 14:16:29 -0500

    refactor: Convert SVG icons to components

--- frontend/src/assets/icons.ts
+++ frontend/src/assets/icons.ts
@@ -4,16 +4,6 @@ const Icons = {
 	logo
 };
 
-import Github from './icons/github.svg';
-import Mastodon from './icons/mastodon.svg';
-import Email from './icons/email.svg';
-
-const Brands = {
-	Github,
-	Mastodon,
-	Email
-};
-
-export { Icons, Brands };
+export { Icons };
 
 export * from './icons';
