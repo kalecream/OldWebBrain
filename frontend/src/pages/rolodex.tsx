406fa096a8b73f660cab878316c60193fa615b1b
kalecream <sabrinamedwinter@gmail.com>
2024-02-06 07:53:26 -0500

    refactor: Map rolodex links

--- frontend/src/pages/rolodex.tsx
+++ frontend/src/pages/rolodex.tsx
@@ -1,19 +1,66 @@
-import Page from "@containers/layout/page";
-import Link from "next/link";
+import Page from '@containers/layout/page';
+import Link from 'next/link';
+import style from '@styles/modules/Rolodex.module.scss';
+
+interface BookmarkProps {
+	url: string;
+	title?: string;
+	description?: string;
+	tags?: string[];
+	type: string;
+}
+
+const Bookmarks: BookmarkProps[] = [
+	{
+		url: 'https://www.wilsonquarterly.com/quarterly/_/whats-the-best-way-to-die',
+		title: "What's the best way to die?",
+		description: 'An article I found on physician-assisted suicide.',
+		tags: ['death', 'suicide'],
+		type: 'article',
+	},
+	{
+		url: 'https://grimgrains.com/site/home.html',
+		title: 'Grim Grains',
+		description: 'A vegan cooking blog with cute illustrations. I\'ve been thinking about blogging about exploring cooking and exploring making a fallback cookbook for myself in a similar format, but I don\'t want to be a copycat.',
+		tags: ['cooking'],
+		type: 'website',
+	},
+	{
+		url: 'https://jzhao.xyz/posts/a-failure-resume',
+		title: 'Failure Resume',
+        tags: [''],
+        description: "\"A failure resume isn’t just a showcase of failure in and of itself, but also a document of all the spectacular ways you’ve worked hard towards your own goals and accomplishments. In many ways, it validates your struggle and your effort.\"",
+		type: 'article',
+	},
+	{
+		url: 'https://memo.barrucadu.co.uk/personal-finance.html',
+		title: 'Personal Finance',
+		tags: [''],
+        description: "I had been looking up ways persons were using Ledger. Instead I got a paradigm shift on how I was viewing my personal finances and a cool new blog to follow. I now use beancount, but I come back to this post every now and again. The calculations for short and long runaway (i.e. when we run out of money) are very helpful to consider .",
+        type: 'webpage'
+	},
+];
+
+// TODO: filter by tags and type
 
 export const Rolodex = () => {
-    return (
-        <Page>
-            <section style={{textAlign: 'left'}}>
-                <h2 className='section-title'>Rolodex</h2>
-                    <p>
-                        <Link href="https://grimgrains.com/site/home.html" target="_blank">Grim Grains</Link> Vegan recipe blog with cute illustrations.
-                    </p>
-                    <p><Link href="https://jzhao.xyz/posts/a-failure-resume" target="_blank">Failure Resume</Link></p>
-                    <p><Link href="https://memo.barrucadu.co.uk/personal-finance.html" target="_blank">Personal Finance</Link> I had been looking up ways persons were using Ledger. Instead I got a paradigm shift on how I was viewing my personal finances and a cool new blog to follow. I now use beancount, but I come back to this post every now and again to make sure my run-aways are correct. Super-duper important need-to-know information if you ever lose your job.</p>
-            </section>
-        </Page>
-    );
-}
+	return (
+		<Page>
+			<section style={{ textAlign: 'left' }}>
+				<h2 className="section-title">Rolodex</h2>
+				<div className={style.container + ` pancake`}>
+                    {Bookmarks.map((bookmark) => (
+                        <div className={style.link}>
+						<Link href={bookmark.url} target="_blank" key={bookmark.url} className={style.link + `pancake-child`}>
+							<h3>{bookmark.title}</h3>
+							<p>{bookmark.description}</p>
+                            </Link>
+                            </div>
+					))}
+				</div>
+			</section>
+		</Page>
+	);
+};
 
 export default Rolodex;
