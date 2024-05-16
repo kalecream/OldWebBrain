6ceda24043ef46bb596289c30477d9e18dea0ab5
kalecream <sabrinamedwinter@gmail.com>
2024-05-16 08:58:18 -0500

    chore: update now page

--- frontend/src/pages/now.tsx
+++ frontend/src/pages/now.tsx
@@ -1,4 +1,5 @@
 import { Page } from '@containers/layout';
+import style from '@styles/modules/Now.module.scss';
 import Link from 'next/link';
 
 const NowPage = () => {
@@ -6,29 +7,37 @@ const NowPage = () => {
 		<Page>
 			<section>
 				<h2>Now.</h2>
-				
 
-                <div className='prose'>
-                <p>
-					{' '}
-					The idea of a now page came from <Link href={"https://sive.rs/now3"}>Derek Sivers</Link>. A page I stumbled on while trying to see which web-rings were still active.
-				</p>
-					<h3>2024-Q1</h3>
-                    <p>There aren't exactly goals, but a bunch of things/experiences I'm trying to work on. I'll likely update the points with pictures and links to show progress.</p>
-                    <ul>
-                        <li>Studying how to communicate effectively, consideration and love.</li>
-                        <li>Studying and exploring combatting individualism vs caring for self as a member of a community.</li>
-                        <li>Expressing myself through cooking, baking, 3D modelling, and painting with acrylics and possibly moving to a graphic journal instead of a just a text journal.</li>
-                        <li>A v1.0.0 version of this website.</li>
-                    </ul>
-                </div>
-                {/* TODO: style past */}
+				<div className="prose">
+					<p>
+						{' '}
+						The idea of a now page came from <Link href={'https://sive.rs/now3'}>Derek Sivers</Link>. A page I stumbled
+						on while trying to see which web-rings were still active.
+                    </p>
+                    <div className='prose'>
+                        <h3 className={style.heading}>🌱 2024-Spring</h3>
+                        <p>
+						These aren't exactly goals, but a collection of things/experiences I'm trying to complete. I'll likely update the points with a blog post to show progress.
+					</p>
+					<p>
+						<ul>
+							
+                            <li>Exploring individualism vs caring for self as a member of a community.</li>
+                            <li>Completing my guide on my relationship with myself and others.</li>
+                            <li>Advancing in the C25K program.</li>
+                            <li>A v1.0.0 version of this website.</li>
+                            <li>Creating a GBStudio game as a love letter to a muse.</li>
+						</ul>
+                        </p>
+                        </div>
+				</div>
+				{/* TODO: style past */}
 				<div>
-                    <details>
-                        <summary>Behind Me.</summary>
-                        <p>A clean slate.</p>
-                    </details>
-                </div>
+                    <details className={style.behind}>
+						<summary>Behind Me.</summary>
+						<p>A clean slate.</p>
+					</details>
+				</div>
 			</section>
 		</Page>
 	);
