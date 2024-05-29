4a950a2cdd0f0547590549b4480bf644aef5010b
kalecream <sabrinamedwinter@gmail.com>
2024-05-29 10:07:52 -0500

    refactor: syntax cleanup of blogposts

--- frontend/src/app/components/blogroll.tsx
+++ frontend/src/app/components/blogroll.tsx
@@ -33,87 +33,92 @@ const monthNames = [
 	'December',
 ];
 
-
 // async function Views({ slug }: { slug: string }) {
 //     let views = await getViewsCount();
-  
+
 //     return <ViewCounter allViews={views} slug={slug} />;
 //   }
-  
 
 export const BlogPosts = () => {
-    let posts = getBlogPosts();
-  
-        posts && posts.forEach((post) => {
-            const [yearDate, monthDate, dayDate] = post.metadata.date.split(' ')[0].split('-');
-  
-            let year: Year | undefined = years.find((year) => year.date === yearDate);
-            if (!year) {
-                year = {
-                    date: yearDate,
-                    months: [],
-                };
-                years.push(year);
-            }
-  
-            let month = year.months.find((month) => month.date === monthDate);
-            if (!month) {
-                const monthIndex = parseInt(monthDate) - 1;
-                const monthName = monthNames[monthIndex];
-                month = {
-                    date: monthDate,
-                    name: monthName,
-                    posts: [],
-                };
-                year.months.push(month);
-            }
-  
-            month.posts.push({
-                ...post,
-                date: dayDate,
-            });
+	let posts = getBlogPosts();
+
+    useMemo(() => {
+	posts &&
+		posts.forEach((post) => {
+			const [yearDate, monthDate, dayDate] = post.metadata.date.split(' ')[0].split('-');
+
+			let year: Year | undefined = years.find((year) => year.date === yearDate);
+			if (!year) {
+				year = {
+					date: yearDate,
+					months: [],
+				};
+				years.push(year);
+			}
+
+			let month = year.months.find((month) => month.date === monthDate);
+			if (!month) {
+				const monthIndex = parseInt(monthDate) - 1;
+				const monthName = monthNames[monthIndex];
+				month = {
+					date: monthDate,
+					name: monthName,
+					posts: [],
+				};
+				year.months.push(month);
+			}
+
+			month.posts.push({
+				...post,
+				date: dayDate,
+			});
+		});
+
+	years
+		.sort((a, b) => parseInt(b.date) - parseInt(a.date))
+		.forEach((year) => {
+			year.months
+				.sort((a, b) => parseInt(b.date) - parseInt(a.date))
+				.forEach((month) => {
+					month.posts.sort((a, b) => parseInt(b.date) - parseInt(a.date));
+				});
         });
-  
-        years
-            .sort((a, b) => parseInt(b.date) - parseInt(a.date))
-            .forEach((year) => {
-                year.months
-                    .sort((a, b) => parseInt(b.date) - parseInt(a.date))
-                    .forEach((month) => {
-                        month.posts.sort((a, b) => parseInt(b.date) - parseInt(a.date));
-                    });
-            });
-  
-    return (
-      <section>
-        <h2>Blog</h2>
-        { years.map((year) => (
-                          <ul key={year.date}>
-                          <li className={blog.nolist} key={year.date}>
-                              {year.months.map((month, i) => (
-                                  <ul key={i}>
-                                      <li className={blog.nolist}>
-                                          <h2 className={blog.month}>
-                                              <span>{month.name}</span> <span>{i === 0 && year.date}</span>
-                                          </h2>
-                                      </li>
-                                      {month.posts.map((post,i) => (
-                                          <Link className={blog.link} href={`/blog/${post.slug}`} key={i}>
-                                              <div className={blog.list__section}>
-                                                  <div>
-                                                      <span className={blog.list__date}>{post.metadata.date.slice(-2)}</span>
-                                                      <h3 className={blog.list__title}>{post.metadata.title}</h3>
-                                                  </div>
-                                                  <p className={blog.list__description}>{post.metadata.description}</p>
-                                              </div>
-                                          </Link>
-                                      ))}
-                                  </ul>
-                              ))}
-                          </li>
-                          </ul>
-                      ))}
-      </section>
-    )
-  
-  }
+        
+    }, [posts])
+
+	return (
+		<section>
+			<h2>Blog</h2>
+			{years.map((year) => (
+				<ul key={year.date}>
+					<li className={blog.nolist} key={year.date}>
+						<ul>
+							{year.months.map((month, i) => (
+								<li className={blog.nolist} key={i}>
+									<h2 className={blog.month}>
+										<span>{month.name}</span> <span>{i === 0 && year.date}</span>
+									</h2>
+									<ul>
+										{month.posts.map((post) => (
+											<li key={post.slug} className='no-marker'>
+												<Link className={blog.link} href={`/blog/${post.slug}`}>
+													<div className={blog.list__section}>
+														<div>
+															<span className={blog.list__date}>{post.metadata.date.slice(-2)}</span>
+															<h3 className={blog.list__title}>{post.metadata.title}</h3>
+														</div>
+														<p className={blog.list__description}>{post.metadata.description}</p>
+													</div>
+												</Link>
+											</li>
+										))}
+									</ul>
+								</li>
+							))}
+						</ul>
+					</li>
+				</ul>
+			))}
+		</section>
+	);
+};
