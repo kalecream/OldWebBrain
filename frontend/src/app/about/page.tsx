bc3099d60c9a57bcc0b1663bfb8d3a92038e4ec3
kalecream <sabrinamedwinter@gmail.com>
2024-07-14 07:07:54 -0500

    refactor: change bookshelf layout

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -2,16 +2,18 @@
 import { useEffect, useState } from 'react';
 import Books from '@data/books';
 import Pods from '@data/pod';
-import CurrentReads from '@components/books/Reads';
-import { BacklogGraph, BookShelf, RandomBooks } from '@components/books/backlogGraph';
+import Reads from '@components/books/Reads';
+import { BacklogGraph, RandomBooks } from '@components/books/backlogGraph';
 // import PhotoGalley from '@components/books/gallery';
 import styles from '@components/books/books.module.scss';
+import button from '@styles/modules/Button.module.scss';
 // import { CodersrankSummary } from '@components/books/CodersRank';
 import Link from 'next/link';
 import Image from 'next/image';
 import style from '@styles/modules/Rolodex.module.scss';
 import pic from '@public/img/album/plants.gif';
 import { ScrollDown } from '@components/scrollDown';
+import { FaArrowLeftLong, FaArrowRightLong } from 'react-icons/fa6';
 
 interface BookmarkProps {
 	url: string;
@@ -109,10 +111,6 @@ export const About = () => {
 		(a, b) => new Date(b.added).getTime() - new Date(a.added).getTime(),
 	);
 
-	const ReadBooks = Books.filter((book) => book.status === 'Read').sort(
-		(a, b) => new Date(b.finished).getTime() - new Date(a.finished).getTime(),
-	);
-
 	return (
 		<section>
 			<h1 className="text-center">About</h1>
@@ -120,13 +118,26 @@ export const About = () => {
 			{/* TODO: Fix photogallery on about */}
 			<section>
 				<div className="flex column center">
-					<Image src={pic} alt="" height={200} width={300} style={{ margin: 'auto' }} />
-					<p className="prose">
+					<Image src={pic} alt="" height={100} width={200} style={{ margin: 'auto' }} />
+					<p className="prose glassmorphic">
 						I made this about page because showing you my personality through the things I enjoy & abhor is personally
 						preferable to describing it. I've always disliked that "tell me a bit about yourself" in personal
-						environments. This page is still under construction. When it is complete, there will be a tldr of my bio,
-						then a map of my facets.
+						environments. This page is still under construction.
 					</p>
+					<div className={`flex row`}>
+						<Link className={button.primary + ` glassmorphic`} href={'#books'} style={{ width: '100px', height: '100px' }}>
+							Books
+						</Link>
+						<Link className={button.primary + ` glassmorphic`} href={'#games'} style={{ width: '100px', height: '100px' }}>
+							Games
+						</Link>
+						<Link className={button.primary + ` glassmorphic`} href={'#podcasts'} style={{ width: '100px', height: '100px' }}>
+							Podcasts
+						</Link>
+						<Link className={button.primary + ` glassmorphic`} href={'#rolodex'} style={{ width: '100px', height: '100px' }}>
+							Rolodex
+						</Link>
+					</div>
 					<ScrollDown />
 				</div>
 			</section>
@@ -144,47 +155,53 @@ export const About = () => {
 
 					<CodersrankSummary /> */}
 
-			<section>
+			<section id="books">
 				<div className={styles.paragraph}>
 					<h2 className="text-center">Books</h2>
-
-					<p>
+					<p className="prose glassmorphic">
 						I like to read to learn about the world around me or get laughs. I have{' '}
-						<b>{Books.length} books in my library</b> (digital and non-digital), and I'm always looking for more. I
-						prefer {Percentage[0] > Percentage[1] ? 'Fiction' : 'Non-Fiction'}, so I read about{' '}
+						<b>{Books.filter((book) => book.status != 'Want').length} books in my library</b>,
+						and I'm always looking for more. I prefer {Percentage[0] > Percentage[1] ? 'Fiction' : 'Non-Fiction'}, so I
+						read about{' '}
 						<b>
 							{Percentage[0].toFixed(1)}% fiction and {Percentage[1].toFixed(1)}% non-fiction
 						</b>
 						.
 					</p>
-
-					<p>
+					<div className="flex row align-items">
+						<Link className="prose glassmorphic text-center" href="/read#read">
+							<FaArrowLeftLong /> {' '}Last
+						</Link>
+						<Reads status="Read" limit={1} />
+						*
+						<Reads status="Want" limit={1} />
+						<Link className="prose glassmorphic text-center" href="/read#want">
+							Next {' '}<FaArrowRightLong />
+						</Link>
+					</div>
+					
+				</div>
+					<p className="prose glassmorphic">
 						I'm currently reading {Books.filter((book) => book.status === 'Reading').length} books, which you can see
-						below. My 10 most frequently read book tags are: {topGenres}.
+						below. <b>My 10 most frequently read book tags are: {topGenres}.</b>
 					</p>
-
-					<CurrentReads />
-					<p>
+					<Reads status="Reading" />
+					<p className="prose glassmorphic">
 						This graph below is my book status backlog for the past rolling year. This ignores books from before then to
 						ensure that I'm keeping up my desired reading pace of 24 books for every 12 months with 70/30 Fiction to
-						Non-Fiction. I have {wantToReadBooks.length} books in my backlog.
+						Non-Fiction. I have {wantToReadBooks.length} books in my backlog to-read.
 					</p>
-					<BacklogGraph />
-					<p>
-						I show all my to-read and read books below. As well as a random book review or my thoughts on the book. A
-						small heart means that I have it rated highly, while a cross means that I pretty much hated the book. The
-						last book I read was <b>{ReadBooks[0].title}</b>.
-					</p>
+				<BacklogGraph />
+				<h3 className="text-center">Random Review</h3>
 					<RandomBooks />
-					<BookShelf />
-				</div>
+					
 			</section>
 
-			<section className={styles.paragraph}>
+			<section className={styles.paragraph} id="games">
 				<div>
 					<h2 className="text-center">Games</h2>
 
-					<p>
+					<p className={`glassmorphic prose`}>
 						My preference is playing indie games and games I physically own. I still play my Gameboy Advance and I own 3
 						different DSes. Not featured below: several untouched games from{' '}
 						<Link href="https://steamcommunity.com/id/SabMedwinter">Steam</Link> and{' '}
@@ -193,12 +210,24 @@ export const About = () => {
 					<div className="flex row">
 						<div className="">
 							<Link href="https://www.legendsofidleon.com/">
-								<Image width={300} height={150} src={'https://imgur.com/APzegNB.png'} alt="IdleOn" />
+								<Image
+									width={300}
+									height={150}
+									src={'https://imgur.com/APzegNB.png'}
+									alt="IdleOn"
+									className={`outerglow`}
+								/>
 							</Link>
 						</div>
 						<div className="">
 							<Link href="https://shatteredpixel.com/shatteredpd/">
-								<Image width={300} height={150} src="https://i.imgur.com/J6ExEz6.gif" alt="Shattered Pixel Dungeon" />
+								<Image
+									width={300}
+									height={150}
+									src="https://i.imgur.com/J6ExEz6.gif"
+									alt="Shattered Pixel Dungeon"
+									className={`outerglow`}
+								/>
 							</Link>
 						</div>
 						<div className="">
@@ -208,17 +237,30 @@ export const About = () => {
 									height={150}
 									src="https://i.imgur.com/JbZW1N3.jpg"
 									alt="Etrian Odyssey II: Heroes of Lagaard"
+									className={`outerglow`}
 								/>
 							</Link>
 						</div>
 						<div className="">
 							<Link href="https://en.wikipedia.org/wiki/The_World_Ends_with_You">
-								<Image width={150} height={150} src="https://i.imgur.com/j6DsuvT.jpeg" alt="World Ends With You" />
+								<Image
+									width={150}
+									height={150}
+									src="https://i.imgur.com/j6DsuvT.jpeg"
+									alt="World Ends With You"
+									className={`outerglow`}
+								/>
 							</Link>
 						</div>
 						<div className="">
 							<Link href="https://en.wikipedia.org/wiki/Advance_Wars">
-								<Image width={150} height={150} src="https://i.imgur.com/ofu3y5f.jpeg" alt="Advanded Wars" />
+								<Image
+									width={150}
+									height={150}
+									src="https://i.imgur.com/ofu3y5f.jpeg"
+									alt="Advanded Wars"
+									className={`outerglow`}
+								/>
 							</Link>
 						</div>
 
@@ -229,14 +271,16 @@ export const About = () => {
 									height={150}
 									src="https://i.imgur.com/NsBAPjW.jpeg"
 									alt="Professor LAyton and The Curious Village"
+									className={`outerglow`}
 								/>
 							</Link>
 						</div>
 					</div>
 				</div>
 			</section>
-			<section>
-				<div className={styles.paragraph}>
+
+			<section className={styles.paragraph} id="podcasts">
+				<div>
 					<h2 className="text-center">Podcasts</h2>
 					<p className="prose glassmorphic">
 						I mainly listen to audiodramas and horror podcasts, but tend to listen to Non-Fiction or Media Podcasts when
@@ -246,8 +290,15 @@ export const About = () => {
 						{Pods.sort((a, b) => a.title.localeCompare(b.title)).map((p) => (
 							<div className="">
 								{p.url ? (
-									<Link href={p.url} >
-										<Image width={125} height={125} src={p.cover} alt={p.title} title={p.title} className={`outerglow`} />
+									<Link href={p.url}>
+										<Image
+											width={125}
+											height={125}
+											src={p.cover}
+											alt={p.title}
+											title={p.title}
+											className={`outerglow`}
+										/>
 									</Link>
 								) : (
 									<small>{p.title}</small>
@@ -257,10 +308,11 @@ export const About = () => {
 					</div>
 				</div>
 			</section>
-			<section>
-				<div className={styles.paragraph}>
+
+			<section className={styles.paragraph} id="rolodex">
+				<div>
 					<h2 className="text-center">Rolodex</h2>
-					<p className="prose">
+					<p className="prose glassmorphic">
 						These are some bookmarks for pages & media I've enjoyed on the net. In the future when I have areas for this
 						site similar to how I map out my life, the links will be moved to respective sectors.
 					</p>
---
+++ frontend/src/app/read/page.tsx
@@ -0,0 +1,11 @@
+import { BookShelf } from "@components/books/backlogGraph";
+
+const Read = () => {
+    return (
+        <section>
+            <BookShelf/>
+        </section>
+    )
+}
+
+export default Read;
