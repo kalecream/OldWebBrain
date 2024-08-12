b4bed9463106f18d2c493333c524652a0ec6ad52
kalecream <sabrinamedwinter@gmail.com>
2024-08-12 13:19:09 -0500

    refactor: add post and other  blocks to index

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,32 @@
 import Hero from '@components/hero/hero';
 import ProjectList from '@components/projects/projectsList';
 import Image from 'next/image';
-import Rolodex from '../../public/img/album/rolodex.png';
-import Grimorie from '../../public/img/album/grimorie.png';
+import Rolodex from '@public/img/album/rolodex.png';
+import Grimorie from '@public/img/album/grimorie.png';
 import Projects from '@data/projectsData';
 import { GetMonthName } from '@utils/GetMonthName';
 import styles from '@styles/modules/projects.module.scss';
+import Link from 'next/link';
+import { BlogPosts } from './components/blogroll';
 // import { PreloadResources } from './preload';
 // import { HomePosts } from './components/blogroll';
 
 export default function Page() {
-	[...Projects].sort((a, b) => new Date(b.created).getTime() - new Date(a.created).getTime());
+	[...Projects]
+		.filter((p) => p.image)
+		.filter((c) => c.category == 'code')
+		.sort((a, b) => new Date(b.created).getTime() - new Date(a.created).getTime());
 
 	return (
 		<section>
 			{/* <PreloadResources /> */}
 			<Hero />
 			{/* <HomePosts /> */}
+			{/* TODO: This as a component and add css */}
 			<section className="flex center">
-				<div>
+				<div
+					style={{ border: 'var(--border)', borderRadius: 'var(--borderRadius)', padding: 'var(--paddingContainer)' }}
+				>
 					<h2 className={styles.project__title}>
 						<div className={styles['project-year']}>
 							{GetMonthName(Projects[0].created) || 'NULL'}
@@ -50,18 +58,70 @@ export default function Page() {
 					/>
 				</div>
 				<div className="flex row" style={{ filter: 'saturate(10%);' }}>
-					<div className="flex column">
-						<Image src={Rolodex} alt={''} width={200} height={200} />
-						<caption>Rolodex</caption>
-					</div>
-					<div className="flex column">
+					<Link href="/rolodex">
+						<div
+							className="flex column"
+							style={{
+								backgroundColor: 'var(--primary)',
+								padding: 'var(--paddingContainer)',
+								borderRadius: 'var(--borderRadius)',
+							}}
+						>
+							<Image src={Rolodex} alt={''} width={200} height={200} />
+							<caption
+								style={{
+									color: 'var(--backgroundColor)',
+								}}
+							>
+								Rolodex
+							</caption>
+						</div>
+					</Link>
+					{/* <div className="flex column">
 						<Image src={Grimorie} alt={''} width={200} height={200} />
 						<caption>Week Logs</caption>
-					</div>
-					<div className="flex column">
-						<Image src={Rolodex} alt={''} width={200} height={200} />
-						<caption>Site ChangeLogs</caption>
-					</div>
+					</div> */}
+					<Link href="/changelog">
+						<div
+							className="flex column"
+							style={{
+								backgroundColor: 'var(--primary)',
+								padding: 'var(--paddingContainer)',
+								borderRadius: 'var(--borderRadius)',
+							}}
+						>
+							<Image src={Grimorie} alt={''} width={200} height={200} />
+							<caption
+								style={{
+									color: 'var(--backgroundColor)',
+								}}
+							>
+								Changelog
+							</caption>
+						</div>
+					</Link>
+					<Link href="https://notes.sabrinamedwinter.com">
+						<div
+							className="flex column"
+							style={{
+								backgroundColor: 'var(--primary)',
+								padding: 'var(--paddingContainer)',
+								borderRadius: 'var(--borderRadius)',
+							}}
+						>
+							<Image src={Grimorie} alt={''} width={200} height={200} />
+							<caption
+								style={{
+									color: 'var(--backgroundColor)',
+								}}
+							>
+								Wiki
+							</caption>
+						</div>
+					</Link>
+				</div>
+				<div>
+					<BlogPosts />
 				</div>
 			</section>
 			<ProjectList />
