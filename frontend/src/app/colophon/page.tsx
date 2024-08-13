a88b4410dbe105425351612a856b72f55aabbb72
kalecream <sabrinamedwinter@gmail.com>
2024-08-13 13:12:48 -0500

    feat: add colophon page

---
+++ frontend/src/app/colophon/page.tsx
@@ -0,0 +1,18 @@
+const ColophonPage = () => {
+	return (
+		<section className="paragraph">
+			<h1>Colophon</h1>
+			<div className="prose"><p>
+				Colophon A colophon is a publishing term that describes how a book was made. This is that, but for a website.{' '}
+			</p>
+			<p>This website is made using Next.js, stored on Github and deployed using Vercel.</p></div>
+			
+			<div className="prose">
+				<h2>Credits</h2>
+				<p>To be added.</p>
+			</div>
+		</section>
+	);
+};
+
+export default ColophonPage;
