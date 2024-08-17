2e256bccfd4b8aef5aea725d2fe70d7f948e979b
kalecream <sabrinamedwinter@gmail.com>
2024-08-17 13:11:12 -0500

    chore: update sitemap

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -3,10 +3,10 @@ import { getBlogPosts } from '@app/db/blog';
 export const sitemap = async () => {
 	let blogs = getBlogPosts().map((post) => ({
 		url: `https://sabrinamedwinter.com/blog/${post.slug}`,
-		lastModified: post.metadata.date,
+		lastModified: post.metadata.update || post.metadata.date,
 	}));
 
-	let routes = ['/', '/blog', '/changelog', '/work'].map((route) => ({
+	let routes = ['/', '/blog', '/changelog', '/colophon', '/c25k','/now','/read','/rolodex','/services'].map((route) => ({
 		url: `https://sabrinamedwinter.com${route}`,
 		lastModified: new Date().toISOString().split('T')[0],
 	}));
