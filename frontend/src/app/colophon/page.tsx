1882ec5be60d80e53f4ea37a5916bc574ae87dbd
kalecream <sabrinamedwinter@gmail.com>
2024-08-18 08:25:59 -0500

    chore: update colophon

--- frontend/src/app/colophon/page.tsx
+++ frontend/src/app/colophon/page.tsx
@@ -5,15 +5,15 @@ const ColophonPage = () => {
 		<section className="paragraph">
 			<h1>Colophon</h1>
 			<div className="prose"><p>
-				Colophon A colophon is a publishing term that describes how a book was made. This is that, but for a website.{' '}
+				A colophon is a publishing term that describes how a book was made. This is that, but for a website.{' '}
 			</p>
-			<p>This website is made using Next.js, stored on Github and deployed using Vercel.</p></div>
+			<p>This website is made using Next.js 14, stored on Github and deployed using Vercel.</p></div>
 			
 			<div className="prose">
 				<h2>Credits</h2>
 				<ul>
-					<li><Link href={"https://www.rpnation.com/threads/fifteen-years-later-the-vampire-diaries-the-originals-rp.240860/"} target="_blank">Vampire Post Divider</Link> found on a  roleplaying forum discussing The Vampire Diaries/The Originals</li>
-					<li><Link href={"https://codepen.io/sosuke/pen/Pjoqqp"} target="_blank">CSS Filter Generator</Link> to change colors from Black to a specific Hex</li>
+					<li><Link href={"https://www.rpnation.com/threads/fifteen-years-later-the-vampire-diaries-the-originals-rp.240860/"} target="_blank">Vampire Post Divider</Link> found on a  roleplaying forum discussing The Vampire Diaries/The Originals.</li>
+					<li><Link href={"https://codepen.io/sosuke/pen/Pjoqqp"} target="_blank">CSS Filter Generator</Link> to change colors from Black to a specific Hex. This is used to change the blog title when hovering (on non-index pages.)</li>
 				</ul>
 			</div>
 		</section>
