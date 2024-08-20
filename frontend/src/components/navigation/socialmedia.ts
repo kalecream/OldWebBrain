b5879a53bf5fe0422639b833615e87052478b8ab
kalecream <sabrinamedwinter@gmail.com>
2024-08-20 16:38:27 -0500

    chore: add credits to button

--- frontend/src/components/navigation/socialmedia.ts
+++ frontend/src/components/navigation/socialmedia.ts
@@ -2,20 +2,10 @@ import { FaCoffee } from 'react-icons/fa';
 import { FaTwitter, FaGithub, FaLinkedin, FaCodepen, FaRss, FaEnvelope } from 'react-icons/fa6';
 
 export const MediaProfiles = [
-	// {
-	// 	name: 'LinkedIn',
-	// 	url: 'https://www.linkedin.com/in/medwinter/',
-	// 	icon: FaLinkedin
-	// },
 	{
-		name: 'Github',
-		url: 'https://github.com/kalecream',
-		icon: FaGithub,
-	},
-	{
-		name: 'Codepen',
-		url: 'https://codepen.io/medwinter',
-		icon: FaCodepen,
+		name: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/medwinter/',
+		icon: FaLinkedin
 	},
 	{
 		name: 'Twitter',
