95124f99501e6b8bb634d694be5eeaecf7283af6
kalecream <sabrinamedwinter@gmail.com>
2024-08-19 19:20:14 -0500

    styling: fixes to changelog

--- frontend/src/app/changelog/page.tsx
+++ frontend/src/app/changelog/page.tsx
@@ -39,45 +39,55 @@ const GitHubReleaseInfo = () => {
 	}, []);
 
 	return (
-		<section>
-			<h1 className="section-title">Website Changes!</h1>
-			<p>If you would like to see more on how this website was made, you can read <Link href="/colophon" className='internal-link'>colophon</Link>.</p>
-			<div className={styles.release__container}>
-				<Suspense fallback={<p className="h-6" />}>
-					<ul className={styles.release}>
-						{releases.map((release) => (
-							<li key={release.id} className={styles.release__content}>
-								<div className={styles.release__timeline}>
-									<svg
-										className="octicon octicon-commit position-absolute left-0 color-bg-default color-fg-muted"
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 24 24"
-										width="28"
-										height="28"
-									>
-										<path
-											fill-rule="evenodd"
-											d="M15.5 11.75a3.5 3.5 0 11-7 0 3.5 3.5 0 017 0zm1.444-.75a5.001 5.001 0 00-9.888 0H2.75a.75.75 0 100 1.5h4.306a5.001 5.001 0 009.888 0h4.306a.75.75 0 100-1.5h-4.306z"
-											fill="currentColor"
-										></path>
-									</svg>
-								</div>
-								<div className={styles.release__details}>
-									<div className={styles.release__left}>
-										<h2>{release.name} </h2>
-										<small>{format(parseISO(release.published_at), 'MMMM dd, yyyy')}</small>
+		<>
+			<section>
+				<h1>Website Changes!</h1>
+				<p className="prose">
+					If you would like to see more on how this website was made, you can read{' '}
+					<Link href="/colophon" className="internal-link">
+						colophon
+					</Link>
+					.
+				</p>
+			</section>
+			<section>
+				<div className={styles.release__container}>
+					<Suspense fallback={<p className="h-6" />}>
+						<ul className={styles.release}>
+							{releases.map((release) => (
+								<li key={release.id} className={styles.release__content}>
+									<div className={styles.release__timeline}>
+										<svg
+											className="octicon octicon-commit position-absolute left-0 color-bg-default color-fg-muted"
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 24 24"
+											width="28"
+											height="28"
+										>
+											<path
+												fill-rule="evenodd"
+												d="M15.5 11.75a3.5 3.5 0 11-7 0 3.5 3.5 0 017 0zm1.444-.75a5.001 5.001 0 00-9.888 0H2.75a.75.75 0 100 1.5h4.306a5.001 5.001 0 009.888 0h4.306a.75.75 0 100-1.5h-4.306z"
+												fill="currentColor"
+											></path>
+										</svg>
 									</div>
+									<div className={styles.release__details}>
+										<div className={styles.release__left}>
+											<h1>{release.name}</h1>
+											<p>{format(parseISO(release.published_at), 'MMMM dd, yyyy')}</p>
+										</div>
 
-									<div className={styles.release__right}>
-										<MarkdownRender markdown={release.body} />
+										<div className={styles.release__right}>
+											<MarkdownRender markdown={release.body} />
+										</div>
 									</div>
-								</div>
-							</li>
-						))}
-					</ul>
-				</Suspense>
-			</div>
-		</section>
+								</li>
+							))}
+						</ul>
+					</Suspense>
+				</div>
+			</section>
+		</>
 	);
 };
 
---
+++ frontend/src/app/projects/page.tsx
@@ -0,0 +1,27 @@
+import Projects from '@data/projectsData';
+import Image from 'next/image';
+import Link from 'next/link';
+import { extractCategories } from '@components/projects';
+
+const ProjectsPage = () => {
+	const categories = extractCategories();
+
+	return (
+		<>
+			<section>
+				<h1>Projects</h1>
+				<h2>Sundrunk Creature of the dark.</h2>
+				<p>
+					This is the directory page to find information on the projects I've done. I have many virtual and physical
+					projects across different discipline. There will be portfolio pages for the different categories and also
+					individual project pages.
+				</p>
+            </section>
+            <section>
+                <div id="dev"></div>
+                <div id="design"></div>
+                <div id="other"></div>
+            </section>
+		</>
+	);
+};
