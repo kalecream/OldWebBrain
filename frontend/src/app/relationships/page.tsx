95953bcaa8ece0b91ce28e39dbbdf35183ee74ea
Sabrina Medwinter <sabrinamedwinter@gmail.com>
2024-08-23 15:34:56 -0500

    chore: change text

--- frontend/src/app/relationships/page.tsx
+++ frontend/src/app/relationships/page.tsx
@@ -4,7 +4,7 @@ const RShipPage = () => {
     return (
         <>
             <section id="toc" className="prose">
-                <center> <i>"A relationship is a commitment to doing the work of becoming a more thoughtful person."</i></center>
+                <center>  <p className="prose"> <i>"A relationship is a commitment to doing the work of becoming a more thoughtful person."</i></p></center>
                 <ol>
                     <li><Link href="#self">With Self</Link></li>
                     <li><Link href="#family">With Family</Link></li>
@@ -100,21 +100,25 @@ const RShipPage = () => {
                     I found it adorable that both 'friendship' and 'free' came from root of 'Love'. However, this brings me no closer to my own definition for a friend. I asked my close friends for their personal definitions, but it was not something they had rigidly defined for themselves outside of people they care about and like to be around.
                 </p>
                 <p className="prose">Friendship, by contrast, is essentially a kind of _relationship grounded in a particular kind of special concern each has for the other as the person she is; and whereas we must make conceptual room for the idea of unrequited love, ==unrequited friendship is senseless.== Consequently, accounts of friendship tend to understand it not merely as a case of reciprocal love of some form (together with mutual acknowledgment of this love), but as essentially involving significant interactions between the friends—as being in this sense a certain kind of relationship. [^2]</p>
-                <p className="prose"><b>Healthy friendships have a balance of giving and taking:</b>
-                    Can you remember the last time you did something thoughtful for a friend? Or they did something for you?
-                    Who normally reaches out first?
-                    If you live a distance apart, who makes the effort to travel?
-                    Who do you spend the majority of your conversations focused on?
-                    Would you call them if you were in trouble? And if you did, do you think they would be there for you?
-                </p>
+                <ol className="prose">
+                    <b>Healthy friendships have a balance of giving and taking:</b>
+                    <li>Can you remember the last time you did something thoughtful for a friend?</li>
+                    <li>Or they did something for you?</li>
+                    <li>Who normally reaches out first?</li>
+                    <li>If you live a distance apart, who makes the effort to travel?</li>
+                    <li>Who do you spend the majority of your conversations focused on?</li>
+                    <li>Would you call them if you were in trouble? And if you did, do you think they would be there for you?</li>
+                </ol>
                 <p className="prose">Does a friend really need to meet the above requirements? I don't think they do all the time. Maybe hit some periodically? It might depend on how close you have been. It would be easier for close friends who have been separated by time/distance to do maintenance mode friendships. I wouldn’t have friendships like that with someone I just met nor would I have the above expectations off the bat. I would let the other person dictate the expectations I should have of them.</p>
                 <p className="prose">I’m noticing though that people tend to both be overfamiliar and refuse to reciprocate vulnerability. It’s like people want your whole story even if there is no material help, comfort or companionship happening through the trials or great times. I’m an open book, but it bothers me how needy, demanding and entitled new acquaintances can be.
                 </p>
-                <h2>Emotional Bonds</h2>
-                <p className="prose">I reject that close friends cannot have an emotional bond similar to that of a romantic relationship. I accept that the partners of my friends will likely see facets of them that I never will and vice versa, but that can still ring true for any of my friends with any of their other friends. That's a fact of life and not a partnership thing. It may be that I cannot give the time investment into that person that a partner would.</p>
-                <h2>A Common Future</h2>
-                <p className="prose">When I try to find an answer on where other persons draw the line of friendship vs partnership, it seems to be a case of sharing common future plans. It doesn't matter if a friend has a completely different life trajectory in mind, while a partner is expected (?) to be on a similar path. I'm not sure if I agree or disagree yet. It does seem like it would make things more simple if the partners are on a similar path.</p>
-                <p className="prose">I need to explore the thought: a partner doesn't have a similar life path, but a synergistic path like someone who is traveling the world with a long-distance partner who really likes their space.</p>
+                <div>
+                    <h2>Emotional Bonds</h2>
+                    <p className="prose">I reject that close friends cannot have an emotional bond similar to that of a romantic relationship. I accept that the partners of my friends will likely see facets of them that I never will and vice versa, but that can still ring true for any of my friends with any of their other friends. That's a fact of life and not a partnership thing. It may be that I cannot give the time investment into that person that a partner would.</p>
+                    <h2>A Common Future</h2>
+                    <p className="prose">When I try to find an answer on where other persons draw the line of friendship vs partnership, it seems to be a case of sharing common future plans. It doesn't matter if a friend has a completely different life trajectory in mind, while a partner is expected (?) to be on a similar path. I'm not sure if I agree or disagree yet. It does seem like it would make things more simple if the partners are on a similar path.</p>
+                    <p className="prose">I need to explore the thought: a partner doesn't have a similar life path, but a synergistic path like someone who is traveling the world with a long-distance partner who really likes their space.</p>
+                </div>
             </section>
             <section id="romance">
                 <h1>With Romantic Interests</h1>
@@ -150,17 +154,17 @@ const RShipPage = () => {
                 </div>
 
                 <div className="prose">
-                <ol>
-                    <li>offering unwavering support during challenges, personal growth, and ambitions: providing encouragement during setbacks, and celebrating achievements.</li>
-                    <li>allowing them to feel secure enough to share deepest fears, desires, and secrets, knowing they will be met with understanding and acceptance.</li>
-                    <li>respecting the other's individuality and boundaries.
-                    </li>
-                    <li>valuing your opinions, feelings, and needs as much as your own.</li>
-                    <li>open and honest communication.</li>
-                    <li>a willingness to compromise, forgive, and grow from experiences.</li>
-                    <li>maintaining a close physical connection and being attuned to each other’s emotional needs.
-                    </li>
-                </ol>
+                    <ol>
+                        <li>offering unwavering support during challenges, personal growth, and ambitions: providing encouragement during setbacks, and celebrating achievements.</li>
+                        <li>allowing them to feel secure enough to share deepest fears, desires, and secrets, knowing they will be met with understanding and acceptance.</li>
+                        <li>respecting the other's individuality and boundaries.
+                        </li>
+                        <li>valuing your opinions, feelings, and needs as much as your own.</li>
+                        <li>open and honest communication.</li>
+                        <li>a willingness to compromise, forgive, and grow from experiences.</li>
+                        <li>maintaining a close physical connection and being attuned to each other’s emotional needs.
+                        </li>
+                    </ol>
                 </div>
 
                 <p className="prose"><i>
