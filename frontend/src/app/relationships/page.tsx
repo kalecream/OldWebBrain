1f551ba4a3bd16c0b83a159b66ad7f012f61e490
kalecream <sabrinamedwinter@gmail.com>
2024-08-31 14:08:13 -0500

    chore: minor edits n rship

--- frontend/src/app/relationships/page.tsx
+++ frontend/src/app/relationships/page.tsx
@@ -3,29 +3,32 @@ import Link from 'next/link';
 const RShipPage = () => {
 	return (
 		<>
-            <section id="toc">
-                <h1>Introduction</h1>
-				
+			<section id="toc">
+				<h1>Introduction</h1>
 			</section>
-            <section id="introduction">
-            <q>
-					A relationship is a commitment<wbr/> to doing the work of<wbr/> becoming a more thoughtful person.
+			<section id="introduction">
+				<q>
+					A relationship is a commitment
+					<wbr /> to doing the work of
+					<wbr /> becoming a more thoughtful person.
 				</q>
 				<p className="prose">
-					The most daunting problem I’ve found in trying to understand how other persons view relationships is that
-					information on the internet about growing and repairing relationships are geared towards romantic
-					relationships when familial ties and camaraderie are the basis of most of the relationships in our lives.
-				</p>{' '}
+					The most daunting problem I’ve found in trying to understand how other persons view relationships is the
+					seemingly scarce resources on growing and repairing relationships. Most that I've found on the internet and
+					literature are focused on romantic relationships when familial ties and camaraderie are the basis of most of
+					the relationships in our lives. This is also where I've found the most conflict and I believe you need to
+					build a base of being a good human, relative and/or friend to have "successful" romantic relationships.
+				</p>
 				<p className="prose">
-					Another issue is people just having thought about it at all. There are no definitions or personal boundaries.
-					When I've asked persons about their thoughts on some of the things I've been trying to find my own personal
-					definitions for, I realize we've all been basing our social life on vibes, pop culture and whatever we lacked
-					in our formative years. The alternative isn't better either, some who have thought about it have just
-					swallowed some philosopher’s world-view. I have no heroes, so that approach doesn't gel with me. I want to
-					create my own thoughts on this with my own reasoning. I don't want to do that from scratch and it would make
-					no sense to throw away the thoughts that came before me in this venture. My thoughts are speaking on all
-					relationships unless I specify romantic. I believe that romantic relationships should be an evolution of
-					friendship. One cannot be a good partner without being a good friend to their partner.
+					Another issue is having thought about it at all. There are no definitions or personal boundaries. When I've
+					asked persons about their thoughts on some of the things I've been trying to find my own personal definitions
+					for, I realize we've all been basing our social life on vibes, pop culture and hording whatever we lacked in our
+					formative years. The alternative I've seen isn't better. Some who have thought about it have just swallowed some
+					philosopher’s world-view. I have no heroes, so that approach doesn't gel with me. I want to create my own
+					thoughts on this with my own reasoning. I don't want to do that from scratch and it would make no sense to
+					throw away the thoughts that came before me in this venture. My thoughts are speaking on all relationships
+					unless I specify romantic. I believe that romantic relationships should be an evolution of friendship. One
+					cannot be a good partner without being a good friend to their partner.
 				</p>
 				<p className="prose">
 					This document is ongoing as I navigate my relationship with myself and others. It was started and (the bulk of
@@ -33,23 +36,23 @@ const RShipPage = () => {
 					that came with the dissolution of that relationship, I felt the need to define how I wanted to interact with
 					others. I also thought about how I wanted to go about interacting with myself. I often hear that I’m too harsh
 					with myself.
-                </p>
-                <hr/>
+				</p>
+				<hr />
 				<p className="prose">
 					I've outlined a bunch of things for future me to check in with herself and keep us grounded without being a
-					yam-head or the abusive farmer. The most important thing to note is that: 
-                </p>
-                <ol className="prose">
-                    <li> Relationships are unique and do not
-                        follow the same path.</li>
-                    <li>We all know that life likes to just throw us off the road for no reason therefore adaptability
-                        and consideration are  absolutely key. </li>
-                    <li> Communication is a must.</li>
-                    <li>Vulnerability should be encouraged.</li>
-                    <li>All relationships, including the one with self,
-					require effort and intention to nurture them.</li>
-                </ol>
-                <hr/>
+					yam-head or the abusive farmer. The most important thing to note is that:
+				</p>
+				<ol className="prose">
+					<li> Relationships are unique and do not follow the same path.</li>
+					<li>
+						We all know that life likes to just throw us off the road for no reason therefore adaptability and
+						consideration are absolutely key.{' '}
+					</li>
+					<li> Communication is a must.</li>
+					<li>Vulnerability should be encouraged.</li>
+					<li>All relationships, including the one with self, require effort and intention to nurture them.</li>
+				</ol>
+				<hr />
 				<p className="prose">
 					The most important part of a relationship for me is Consideration. This includes considering how my actions
 					affect myself and other persons to: reduce the chance of causing harm, never intentionally causing harm,
@@ -58,23 +61,22 @@ const RShipPage = () => {
 				<p className="prose">
 					I hope to make myself proud by sticking to my values of being considerate and taking a stand, where necessary,
 					to reduce harm in my circle of loved ones, then my wider community.
-                </p>
-                <ol className="prose" style={{textAlign: 'left'}}>
-				<li>
-					<Link href="/relationships/with-self">With Self</Link>
-				</li>
-				<li>
-					<Link href="/relationships/with-family">With Family</Link>
-				</li>
-				<li>
-					<Link href="/relationships/with-friends">With Friends</Link>
-				</li>
-				<li>
-					<Link href="/relationships/with-romance">With Romantic Interests</Link>
-				</li>
-			</ol>
+				</p>
+				<ol className="prose" style={{ textAlign: 'left' }}>
+					<li>
+						<Link href="/relationships/with-self">With Self</Link>
+					</li>
+					<li>
+						<Link href="/relationships/with-family">With Family</Link>
+					</li>
+					<li>
+						<Link href="/relationships/with-friends">With Friends</Link>
+					</li>
+					<li>
+						<Link href="/relationships/with-romance">With Romantic Interests</Link>
+					</li>
+				</ol>
 			</section>
-			
 
 			<section id="influences"></section>
 			<section id="references"></section>
