56dc5680fd45266f429809f5affd5c6eeb143850
kalecream <sabrinamedwinter@gmail.com>
2024-09-01 06:34:56 -0500

    chore: update sitemap

--- frontend/src/data/routes.ts
+++ frontend/src/data/routes.ts
@@ -8,92 +8,97 @@ export const PageRoutes: PageRoutesProps[] = [
     {
         route: '/',
         description: 'Home Page',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/about',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/amateur-radio',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/blog',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/c25k',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/changelog',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/colophon',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/contact',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/now',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/professional',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/projects',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/read',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/relationships',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/relationships/with-family',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/relationships/with-friends',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/relationships/with-romance',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/relationships/with-self',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
 
     },
     {
         route: '/relationships/skip',
-        date: new Date(2024, 8, 31),
+        date: new Date(2024, 7, 31),
+
+    },
+    {
+        route: '/relationships/sitemap',
+        date: new Date(2024, 8, 1),
 
     },
     
