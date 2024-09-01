ca38f47b7434c2c8b2c6d49f3022c97a0c0af515
kalecream <sabrinamedwinter@gmail.com>
2024-09-01 07:10:44 -0500

    chore: add priority to routes

--- frontend/src/data/routes.ts
+++ frontend/src/data/routes.ts
@@ -2,103 +2,123 @@ export interface PageRoutesProps {
     route: string;
     date: Date;
     description?: string;
+    priority: number;
 }
 
-export const PageRoutes: PageRoutesProps[] = [
+export const PageRoutes:PageRoutesProps[] = [
     {
         route: '/',
-        description: 'Home Page',
         date: new Date(2024, 7, 31),
+        description: 'Index',
+        priority: 1.0,
 
     },
     {
         route: '/about',
         date: new Date(2024, 7, 31),
+        priority: 0.4,
 
     },
     {
         route: '/amateur-radio',
         date: new Date(2024, 7, 31),
+        priority: 0.3,
 
     },
     {
         route: '/blog',
         date: new Date(2024, 7, 31),
+        priority: 0.5,
 
     },
     {
         route: '/c25k',
         date: new Date(2024, 7, 31),
+        priority: 0.1,
 
     },
     {
         route: '/changelog',
         date: new Date(2024, 7, 31),
+        priority: 0.8,
 
     },
     {
         route: '/colophon',
         date: new Date(2024, 7, 31),
+        priority: 0.3,
 
     },
     {
         route: '/contact',
         date: new Date(2024, 7, 31),
+        priority: 0.8,
 
     },
     {
         route: '/now',
         date: new Date(2024, 7, 31),
+        priority: 0.4,
 
     },
     {
         route: '/professional',
         date: new Date(2024, 7, 31),
+        priority: 0.4,
 
     },
     {
         route: '/projects',
         date: new Date(2024, 7, 31),
+        priority: 0.8,
 
     },
     {
         route: '/read',
         date: new Date(2024, 7, 31),
+        priority: 0.2,
 
     },
     {
         route: '/relationships',
         date: new Date(2024, 7, 31),
+        priority: 0.4,
 
     },
     {
         route: '/relationships/with-family',
         date: new Date(2024, 7, 31),
+        priority: 0.4,
 
     },
     {
         route: '/relationships/with-friends',
         date: new Date(2024, 7, 31),
+        priority: 0.4,
 
     },
     {
         route: '/relationships/with-romance',
         date: new Date(2024, 7, 31),
+        priority: 0.4,
 
     },
     {
         route: '/relationships/with-self',
         date: new Date(2024, 7, 31),
+        priority: 0.4,
 
     },
     {
         route: '/relationships/skip',
         date: new Date(2024, 7, 31),
+        priority: 0.4,
 
     },
     {
         route: '/relationships/sitemap',
-        date: new Date(2024, 8, 1),
+        date: new Date(2024, 8, 1, 2,7),
+        priority: 0.4,
 
     },
     
