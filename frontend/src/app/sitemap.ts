6590b7fd0648c173e7bc7e87e8253381fcfa8f41
kalecream <sabrinamedwinter@gmail.com>
2024-09-01 07:10:28 -0500

    refactor: update dynamic sitemap

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -5,14 +5,21 @@ export const sitemap = async () => {
 	let blogs = getBlogPosts().map((post) => ({
 		url: `https://sabrinamedwinter.com/blog/${post.slug}`,
 		lastModified: post.metadata.update || post.metadata.date,
+		priority: 0.5,
 	}));
 
-	PageRoutes.map((route) => ({
-		url: `https://sabrinamedwinter.com${route.route}`,
-		lastModified: route.date,
+	console.log(PageRoutes)
+
+	let routes = PageRoutes.map((r) => ({
+		url: `https://sabrinamedwinter.com${r.route}`,
+		lastModified: r.date,
+		priority: r.priority
+		
 	}));
 
-	return [...PageRoutes, ...blogs];
+	console.log(PageRoutes)
+
+	return [...routes, ...blogs];
 };
 
 export default sitemap;
