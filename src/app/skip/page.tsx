fe77cd0d567fbf76f5ebd785ef603ba43f390f61
kalecream <sabrinamedwinter@gmail.com>
2024-09-04 09:14:39 -0500

    chore: populate skip page

--- src/app/skip/page.tsx
+++ src/app/skip/page.tsx
@@ -1,9 +1,14 @@
-import { ComingSoon } from '@components/_misc/ComingSoon';
+import Link from 'next/link';
 
 const SkipPage = () => {
 	return (
 		<>
-			<ComingSoon />
+			<section>
+				<p className='prose'>
+					Hi, friend! This is the personal side of my website for my musings and projects that might not jive with the service seeking crowd.
+				</p>
+				<p className='prose'>Right now, I'm doing <Link href="practice/swordtember">Swordtember</Link> and, then <Link href="practice/inktober">Inktober</Link> challenges.</p>
+			</section>
 		</>
 	);
 };
