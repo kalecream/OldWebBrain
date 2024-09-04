deae247c3afb91ff0cd91db1e1dedbef8cb9a21f
kalecream <sabrinamedwinter@gmail.com>
2024-09-04 13:14:45 -0500

    chore: update skip

--- src/app/skip/page.tsx
+++ src/app/skip/page.tsx
@@ -4,10 +4,11 @@ const SkipPage = () => {
 	return (
 		<>
 			<section>
+				<h1>Hi, Friend!</h1>
 				<p className='prose'>
-					Hi, friend! This is the personal side of my website for my musings and projects that might not jive with the service seeking crowd.
+					This is the personal side of my website for my musings and projects that might not jive with the service-seeking crowd. To this, I'll be adding personal notes, musings and rough drafts of projects that are not polished enough to go in my portfolio.
 				</p>
-				<p className='prose'>Right now, I'm doing <Link href="practice/swordtember">Swordtember</Link> and, then <Link href="practice/inktober">Inktober</Link> challenges.</p>
+				<p className='prose'>Right now, I'm doing <Link href="practice/swordtember">Swordtember</Link> and, then <Link href="practice/inktober">Inktober</Link> challenges to get back into 3D practice and flesh out my portfolio.</p>
 			</section>
 		</>
 	);
