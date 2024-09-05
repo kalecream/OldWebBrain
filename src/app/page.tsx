9ec2ae5f8ae13599deabbe85ef33318e9c84db8d
kalecream <sabrinamedwinter@gmail.com>
2024-09-05 15:32:39 -0500

    chore: removed unused components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 import Hero from '@components/hero/hero';
-import ProjectList from '@components/projects/projectsList';
-import { SecondHero } from '@components/home/SecondHero';
-// import { PreloadResources } from './preload';
-// import { HomePosts } from './components/blogroll';
 
 export default function Page() {
 	
 
 	return (
 		<>
-			{/* <PreloadResources /> */}
 			<Hero />
-			{/* <HomePosts /> */}
-			{/* TODO: This as a component and add css */}
-			{/* <SecondHero />
-			<ProjectList /> */}
 		</>
 	);
 }
