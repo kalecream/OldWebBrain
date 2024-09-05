499c6cf29574b2d500c4adef09d406a2061d2793
kalecream <sabrinamedwinter@gmail.com>
2024-09-05 14:37:42 -0500

    styling: update 404 page style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,9 +3,9 @@ import HeroName from "@components/hero/heroName/heroName";
 const NotFound = () => {
 	return (
 		<section>
-			<HeroName name="Oh no!" />
+			<HeroName name="404" />
 			<p>This page doesn't exist.</p>
-			<p className="prose">If you expected to see something here,<br/> let me know at sabrinamedwinter@gmail.com.</p>
+			<p className="prose text-center">If you expected to see something here,<br/> let me know at sabrinamedwinter@gmail.com.</p>
 		</section>
 	);
 }
