0f3b71b0f5a2f157b91f8cef98cdbbac9d7fcb2a
kalecream <sabrinamedwinter@gmail.com>
2024-09-05 15:29:03 -0500

    chore: update 404 message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,8 @@
-import HeroName from "@components/hero/heroName/heroName";
-
 const NotFound = () => {
 	return (
 		<section>
-			<HeroName name="404" />
-			<p>This page doesn't exist.</p>
-			<p className="prose text-center">If you expected to see something here,<br/> let me know at sabrinamedwinter@gmail.com.</p>
+			<h1>This page doesn't exist (yet?).</h1>
+			<p className="prose"><center>If you expected to see something here,<br/> let me know at sabrinamedwinter@gmail.com.</center></p>
 		</section>
 	);
 }
