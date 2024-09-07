fe91fb30e7260cc2391b7d82152bc4f999e7ab5b
kalecream <sabrinamedwinter@gmail.com>
2024-09-07 11:58:09 -0500

    refactor: re-add preloader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,25 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import RouteLoader from '@pages/preloader';
 import '@styles/global.scss';
 
 export const WEBSITE_HOST_URL = 'https://www.sabrinamedwinter.com';
 
-export const App = ({ Component }: AppProps): JSX.Element => {
+export const App = ({ Component, pageProps }: AppProps): JSX.Element => {
 	return (
-		<ThemeProvider
-			storageKey="theme"
-			attribute="class"
-			enableSystem={true}
-			// If enableSystem is false, the default theme is light
-			defaultTheme="system"
-			themes={['light', 'dark']}
-		>
-			<Component />
-		</ThemeProvider>
+		<>
+			<RouteLoader />
+			<ThemeProvider
+				storageKey="theme"
+				attribute="class"
+				enableSystem={false}
+				// If enableSystem is false, the default theme is light
+				defaultTheme="system"
+				themes={['light', 'dark']}
+			>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
 	);
 };
 
