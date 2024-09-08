c1a4ec8f34fff0cd8ef03a52c11db80820612b5d
kalecream <sabrinamedwinter@gmail.com>
2024-09-08 17:48:23 -0500

    del: remove preloader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
-import RouteLoader from '@pages/preloader';
 import '@styles/global.scss';
 
 export const WEBSITE_HOST_URL = 'https://www.sabrinamedwinter.com';
@@ -8,7 +7,6 @@ export const WEBSITE_HOST_URL = 'https://www.sabrinamedwinter.com';
 export const App = ({ Component, pageProps }: AppProps): JSX.Element => {
 	return (
 		<>
-			<RouteLoader />
 			<ThemeProvider
 				storageKey="theme"
 				attribute="class"
