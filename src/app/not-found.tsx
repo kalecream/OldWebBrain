e579ecce4e75d9a77276e54d38a7c3ec41a8b3eb
kalecream <sabrinamedwinter@gmail.com>
2024-09-08 18:30:44 -0500

    chore: misc minor chnges

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ const NotFound = () => {
 	return (
 		<section className="flex" style={{ minHeight: '80vh' }}>
 			<div>
-				<h1>This page doesn't exist (yet?).</h1>
+				<h1>This page doesn't exist (yet?)</h1>
 				<p className="prose" style={{ textAlign: 'center' }}>
 					If you expected to see something here,
 					<br /> let me know at sabrinamedwinter@gmail.com.
