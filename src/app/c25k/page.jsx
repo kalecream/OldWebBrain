79bb4b71b478ad47a836fe52349e2dac5628fcf4
kalecream <sabrinamedwinter@gmail.com>
2024-09-08 17:06:53 -0500

    feat: Runs split chart

--- src/app/c25k/page.jsx
+++ src/app/c25k/page.jsx
@@ -1,8 +1,10 @@
 'use client';
+import { Suspense } from 'react';
+import Skeleton from 'react-loading-skeleton';
 import Runs from '@data/runs';
-import { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
 import Link from 'next/link';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { WindowWidth } from '@utils/windowDimmensions';
 
 // interface RunProps {
 // 	session: string;
@@ -12,231 +14,76 @@ import Link from 'next/link';
 // 	splits: string[];
 // }
 
-const addTime = (a, b) => {
-	let timeAdded = b.split(':');
-	let ms = (60 * 60 * parseInt(timeAdded[0]) + 60 * parseInt(timeAdded[1])) * 1000;
-
-	var newTime = new Date('1970-01-01T' + a).getTime() + ms;
-	return new Date(newTime).toLocaleString('en-GB').slice(12, 20);
+const parseTimeToSeconds = (time) => {
+	const [minutes, seconds] = time.split(':').map(Number);
+	return minutes * 60 + seconds;
 };
 
-const formatTime = d3.timeParse('%M:%S');
-
-const DistancesChart = () => {
-	const svgRef = useRef();
-	const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-	const h = 300;
-	const w = 750;
-
-	useEffect(() => {
-		Runs.forEach((o) => {
-			o.date = new Date(o.date);
-		});
-
-		const svg = d3.select(svgRef.current).append('svg').attr('width', w).attr('height', h).attr('x', 100);
-
-		const x = d3
-			.scaleTime()
-			.range([margin.left, w - margin.right])
-			.domain(d3.extent(Runs, (d) => d.date))
-			.nice();
-
-		const y = d3
-			.scaleLinear()
-			.range([h - margin.bottom, margin.top])
-			.nice();
-
-		const values = Object.keys(Runs[0]).filter((key) => key !== 'date');
-
-		y.domain([
-			d3.min(Runs, (d) => d3.min(values, (value) => d[value])),
-			d3.max(Runs, (d) => d3.max(values, (value) => d[value])),
-		]);
-
-		const line = d3
-			.line()
-			.x((d) => x(d.date))
-			.y((d) => y(d.distance));
-
-		const paths = values?.map((value) => {
-			return {
-				value: value,
-				path: svg
-					.append('path')
-					.datum(Runs)
-					.attr('fill', 'none')
-					.attr('stroke', 'steelBlue')
-					.attr('stroke-width', 2)
-					.attr(
-						'd',
-						line.y((d) => y(d[value])),
-					),
-			};
-		});
-
-		svg
-			.append('g')
-			.attr('stroke-width', 2)
-			.attr('transform', `translate(0, ${h - margin.bottom})`)
-			.call(d3.axisBottom(x));
-
-		svg.append('g').attr('stroke-width', 2).attr('transform', `translate(${margin.left}, 0)`).call(d3.axisLeft(y));
-	}, []);
-
-	return (
-		<>
-			<div ref={svgRef} className="flex glassmorphic my-1"></div>
-		</>
-	);
+const formatSecondsToTime = (seconds) => {
+	const minutes = Math.floor(seconds / 60);
+	const remainingSeconds = seconds % 60;
+	return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
 };
 
-const TimesChart = () => {
-	const svgRef = useRef();
-	const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-	const h = 300;
-	const w = 750;
+const splitData = Runs.map((run) =>
+	run.splits
+		.filter((split) => split !== '00:00')
+		.map((split, km) => ({
+			session: run.session,
+			km: km + 1,
+			time: parseTimeToSeconds(split),
+		})),
+).flat();
 
-	useEffect(() => {
-		Runs.forEach((o) => {
-			o.date = new Date(o.date);
-			o.avgPace = formatTime(o.avgPace);
-		});
-	}, []);
-
-	const svg = d3.select(svgRef.current).append('svg').attr('width', w).attr('height', h).attr('x', 100);
-
-	const x = d3
-		.scaleUtc()
-		.range([margin.left, w - margin.right])
-		.domain(d3.extent(Runs, (d) => d.date))
-		.nice();
-
-	const y = d3
-		.scaleTime()
-		.range([h - margin.bottom, margin.top])
-		.nice();
-
-	const values = Object.keys(Runs).filter((key) => key == 'avgPace');
-
-	y.domain([
-		d3.min(Runs, (d) => d3.min(values, (value) => d[value])),
-		d3.max(Runs, (d) => d3.max(values, (value) => d[value])),
-	]);
-
-	const line = d3
-		.line()
-		.x((d) => x(d.date))
-		.y((d) => y(d.avgPace));
-
-	const paths = values?.map((value) => {
-		return {
-			value: value,
-			path: svg
-				.append('path')
-				.datum(Runs)
-				.attr('fill', 'none')
-				.attr('stroke', 'steelBlue')
-				.attr('stroke-width', 2)
-				.attr(
-					'd',
-					line.y((d) => y(d[value])),
-				),
-		};
-	});
-
-	svg
-		.append('g')
-		.attr('stroke-width', 2)
-		.attr('transform', `translate(0, ${h - margin.bottom})`)
-		.call(d3.axisBottom(x));
-
-	svg.append('g').attr('stroke-width', 2).attr('transform', `translate(${margin.left}, 0)`).call(d3.axisLeft(y));
+const SplitChart = () => {
+	const width = WindowWidth() > 1000 ? WindowWidth() - 500 : WindowWidth() - 80;
 
 	return (
-		<>
-			<div ref={svgRef} className="flex glassmorphic my-1"></div>
-		</>
+		<LineChart data={splitData} width={width} height={300}>
+			<CartesianGrid strokeDasharray="3 3" />
+			<XAxis
+				dataKey="km"
+				label={{ value: 'Kilometers', position: 'insideBottomRight', offset: 0 }}
+				type="number"
+				ticks={[1, 2, 3, 4, 5]}
+				domain={[1, 5]}
+				allowDecimals={false}
+			/>
+			<YAxis label={{ value: 'Time', angle: -90, position: 'insideLeft' }} tickFormatter={formatSecondsToTime} />
+			<Tooltip labelFormatter={(label) => `KM ${label}`} formatter={(value) => formatSecondsToTime(value)} />
+			{Runs.map((run, index) => (
+				<Line
+					key={index}
+					type="monotone"
+					dataKey="time"
+					data={run.splits.map((split, km) => ({
+						km: km + 1,
+						time: parseTimeToSeconds(split),
+						session: run.session,
+					}))}
+					name={run.session}
+					stroke={`hsl(${(index * 360) / Runs.length}, 70%, 50%)`}
+				/>
+			))}
+		</LineChart>
 	);
 };
 
 const C25KPage = () => {
 	return (
-		<>
-			<h1>Couch to 5KM</h1>
-
-			<section>
-				<div className="flex "></div>
-				<p className="prose glassmorphic">
-					I'm currently using an App called "Zombies, Run 5K Training" to do an 8 week program of the Couch to 5KM
-					challenge. I attempted this briefly before, but I have never made it fully through and would like to improve
-					my running ability.
-				</p>
-				<p className="prose glassmorphic">
-					I used to track my progress via spreadsheet which can be found in the first link below. The second link takes
-					you to the original challenge page:{' '}
-					<Link href="https://docs.google.com/spreadsheets/d/1Ao_9w17kz0X82ZlpYiu4AoUsMOThUjBWkbDFai1bIsA/edit?usp=sharing">
-						Advancing
-					</Link>{' '}
-					in the <Link href="https://c25k.com/c25k_plan/">C25K program</Link>
-				</p>
-
-				<div className="flex column">
-					<TimesChart />
-					<DistancesChart />
-				</div>
-
-				<div className="flex row prose">
-					<fieldset className="flex">
-						<label>Week 1</label>
-						<input type="checkbox" checked readOnly title={Runs[0].date} />
-						<input type="checkbox" />
-						<input type="checkbox" />
-					</fieldset>
-					<fieldset className="flex">
-						<label>Week 2</label>
-						<input type="checkbox" />
-						<input type="checkbox" />
-						<input type="checkbox" />
-					</fieldset>
-					<fieldset className="flex">
-						<label>Week 3</label>
-						<input type="checkbox" />
-						<input type="checkbox" />
-						<input type="checkbox" />
-					</fieldset>
-					<fieldset className="flex">
-						<label>Week 4</label>
-						<input type="checkbox" />
-						<input type="checkbox" />
-						<input type="checkbox" />
-					</fieldset>
-					<fieldset className="flex">
-						<label>Week 5</label>
-						<input type="checkbox" />
-						<input type="checkbox" />
-						<input type="checkbox" />
-					</fieldset>
-					<fieldset className="flex">
-						<label>Week 6</label>
-						<input type="checkbox" />
-						<input type="checkbox" />
-						<input type="checkbox" />
-					</fieldset>
-					<fieldset className="flex">
-						<label>Week 7</label>
-						<input type="checkbox" />
-						<input type="checkbox" />
-						<input type="checkbox" />
-					</fieldset>
-					<fieldset className="flex">
-						<label>Week 8</label>
-						<input type="checkbox" />
-						<input type="checkbox" />
-						<input type="checkbox" />
-					</fieldset>
-				</div>
-			</section>
-		</>
+		<section style={{ minHeight: '80vh' }}>
+			<h1>Couch to 5K</h1>
+			<div className="flex column">
+				<Suspense fallback={<Skeleton count={1} />}>
+					<SplitChart />
+				</Suspense>
+			</div>
+			<p className="prose glassmorphic" style={{ marginTop: '-2rem', marginBottom: '3rem' }}>
+				I'm currently using an App called "Zombies, Run 5K Training" to do an 8 week program of the Couch to 5KM
+				challenge. I attempted this briefly before, but I have never made it fully through and would like to improve my
+				running ability.
+			</p>
+		</section>
 	);
 };
 
