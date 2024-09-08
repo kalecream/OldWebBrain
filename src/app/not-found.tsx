f7e2e417a2fa99ee1304b8835ab39156aaf8d0a6
kalecream <sabrinamedwinter@gmail.com>
2024-09-08 17:40:03 -0500

    styling: center not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,15 @@
 const NotFound = () => {
 	return (
-		<section>
-			<h1>This page doesn't exist (yet?).</h1>
-			<p className="prose"><center>If you expected to see something here,<br/> let me know at sabrinamedwinter@gmail.com.</center></p>
+		<section className="flex" style={{ minHeight: '80vh' }}>
+			<div>
+				<h1>This page doesn't exist (yet?).</h1>
+				<p className="prose" style={{ textAlign: 'center' }}>
+					If you expected to see something here,
+					<br /> let me know at sabrinamedwinter@gmail.com.
+				</p>
+			</div>
 		</section>
 	);
-}
+};
 
 export default NotFound;
