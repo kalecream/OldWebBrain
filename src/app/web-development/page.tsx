5f65799dc3d0755b52b7b4bfb7753ce821aadf0b
kalecream <sabrinamedwinter@gmail.com>
2024-09-10 17:10:49 -0500

    WIP: build form

---
+++ src/app/web-development/page.tsx
@@ -0,0 +1,17 @@
+import { WebDevelopementQuote } from "./WebDevelopementQuote";
+
+export const WebDevelopementPage = () => {
+    return (
+        <>
+            <section>
+                <h1>Featured Projects</h1>
+                <h1>Latest Projects</h1>
+            </section>
+            <section>
+                <WebDevelopementQuote />
+            </section>
+        </>
+    );
+};
+
+export default WebDevelopementPage;
