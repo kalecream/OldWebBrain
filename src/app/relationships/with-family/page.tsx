3a3aac42565d9ec7bd6d7153677235b568097e05
Sab Medwinter <sabrinamedwinter@gmail.com>
2024-09-13 21:26:30 +0000

    with-family

--- src/app/relationships/with-family/page.tsx
+++ src/app/relationships/with-family/page.tsx
@@ -1,7 +1,10 @@
+import { PageReadTime } from "@utils/PageReadTime";
+
 const WithFamilyPage = () => {
   return (
     <section id="family">
       <h1>With Family</h1>
+      <PageReadTime readingSpeedWPM={200} />
       <p className="prose">
         I have not been good at this section. There is something that’s uniquely difficult about maintaining good
         relations with family members. Our first steps into emotional pain and some wounds in our pysche were likely
