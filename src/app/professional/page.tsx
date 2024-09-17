88d85540f45ba90407193764664b8b1fd12c693d
kalecream <sabrinamedwinter@gmail.com>
2024-09-17 12:02:02 -0500

    refactor: restyle service card

--- src/app/professional/page.tsx
+++ src/app/professional/page.tsx
@@ -8,7 +8,7 @@ const ProfessionalPage = () => {
         <h1>Services</h1>
         <p className="prose center" style={{ margin: "-1rem auto" }}>
           If you're interested in any service offered on this page, please look at the portfolio for each below and{" "}
-          <Link href="mailto:sabrinamedwinter.com">reach out to me via email</Link>.
+          <Link href="mailto:sabrinamedwinter@gmail.com">reach out to me via email</Link>.
         </p>
         <ServiceCard />
       </section>
