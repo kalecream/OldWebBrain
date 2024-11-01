92b4ce624483f45064322b9039b6818c3a6f18a2
kalecream <sabrinamedwinter@gmail.com>
2024-11-01 10:13:41 -0500

    styling: change blog post header

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -55,25 +55,28 @@ const formatDate = (date: string) => {
   let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
   let fullDate = new Date(date).toLocaleString("en-us", {
-    month: "short",
+    month: "long",
     day: "numeric",
     year: "numeric",
   });
 
   if (daysAgo < 1) {
     return "Today";
-  } else if (daysAgo < 7) {
-    return `${fullDate} (${daysAgo}d ago)`;
-  } else if (daysAgo < 30) {
-    const weeksAgo = Math.floor(daysAgo / 7);
-    return `${fullDate} (${weeksAgo}w ago)`;
-  } else if (daysAgo < 365) {
-    const monthsAgo = Math.floor(daysAgo / 30);
-    return `${fullDate} (${monthsAgo}mo ago)`;
   } else {
-    const yearsAgo = Math.floor(daysAgo / 365);
-    return `${fullDate} (${yearsAgo}y ago)`;
+    return fullDate;
   }
+  // } else if (daysAgo < 7) {
+  //   return `${fullDate} (${daysAgo}d ago)`;
+  // } else if (daysAgo < 30) {
+  //   const weeksAgo = Math.floor(daysAgo / 7);
+  //   return `${fullDate} (${weeksAgo}w ago)`;
+  // } else if (daysAgo < 365) {
+  //   const monthsAgo = Math.floor(daysAgo / 30);
+  //   return `${fullDate} (${monthsAgo}mo ago)`;
+  // } else {
+  //   const yearsAgo = Math.floor(daysAgo / 365);
+  //   return `${fullDate} (${yearsAgo}y ago)`;
+  // }
 };
 
 export default function Blog({ params }) {
@@ -108,15 +111,17 @@ export default function Blog({ params }) {
         }}
       />
       <div className="info">
-        <h1 className="title">{post.metadata.title}</h1>
-        {/* <h2 className='text-center'>
-					<i >{post.metadata.description}</i>
-        </h2> */}
-
-        <Suspense fallback={<p className="h-5" />}>
-          <PageReadTime readingSpeedWPM={200} />
-          <p className="text-center">{formatDate(post.metadata.date)}</p>
+        <Suspense fallback={<p />}>
+          <p className="text-center">
+            <PageReadTime readingSpeedWPM={200} /> 𑁍 {formatDate(post.metadata.date)}
+          </p>
         </Suspense>
+        <br/><br/>
+        <h1 className="title">{post.metadata.title}</h1>
+        <h2 className="text-center">
+          <i>{post.metadata.description}</i>
+        </h2>
+
         <br />
       </div>
       <article>
