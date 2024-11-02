b28e3f5eb3aa60e0fcb39554735cce19837374b8
kalecream <sabrinamedwinter@gmail.com>
2024-11-02 10:55:01 -0500

    refactor: move personal images to cdn

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,35 +1,109 @@
-import img_20220102 from "./images/3D/2023/plant_20230102_composed.png";
 import Chess from "./images/3D/chess__alternate_view__by_kalecream.jpg";
 import Park from "./images/3D/park_by_kalecream.jpg";
 import UnderConstruction from "./images/Under_Construction_by_kalecream.webp";
 
-const ThreeD = [Chess, Park, img_20220102, UnderConstruction];
+const ThreeD = [Chess, Park, UnderConstruction];
 
 export { ThreeD };
 
-import bananasunset from "./images/album/bananasunset.jpg";
-import bw_trees from "./images/album/bw_trees.jpg";
-import earring from "./images/album/earring.jpg";
-import jellyfish from "./images/album/jellyfish.jpg";
-import lunch_trio from "./images/album/lunch_trio.jpg";
-import okra from "./images/album/okra.jpg";
-import pineapple from "./images/album/pineapple.jpg";
-import plantwindow from "./images/album/plantwindow.jpg";
+// interface GalleryData {
+//   id: number;
+//   src: StaticImageData | string;
+//   name: string;
+//   desc: string;
+//   tags: string[];}
 
-const GPhotos = {
-  bananasunset: bananasunset,
-  bw_trees: bw_trees,
-  earring: earring,
-  jellyfish: jellyfish,
-  lunch_trio: lunch_trio,
-  okra: okra,
-  pineapple: pineapple,
-  plantwindow: plantwindow,
-};
+const GPhotos: GalleryData[] = [
+  {
+    id: 0,
+    src: "https://i.imgur.com/jCQIISQ.jpeg",
+    name: "Banana Sunset",
+    desc: "A banana tree at sunset",
+    tags: ["banana", "sunset"],
+  },
+  {
+    id: 1,
+    src: "https://i.imgur.com/nD4m8JX.jpeg",
+    name: "Black and White Trees",
+    desc: "Trees in black and white",
+    tags: ["trees", "black and white"],
+  },
+  {
+    id: 2,
+    src: "https://imgur.com/Sd2DuQz.jpeg",
+    name: "Earring",
+    desc: "A close-up of an earring",
+    tags: ["earring", "jewelry"],
+  },
+  {
+    id: 3,
+    src: "https://i.imgur.com/I7sOZyW.jpeg",
+    name: "Jellyfish",
+    desc: "A jellyfish in the ocean",
+    tags: ["jellyfish", "ocean"],
+  },
+  {
+    id: 4,
+    src: "https://i.imgur.com/p3ApPPh.jpeg",
+    name: "Lunch Trio",
+    desc: "Three lunch dishes",
+    tags: ["lunch", "food"],
+  },
+  {
+    id: 5,
+    src: "https://i.imgur.com/nIkVHY9.jpeg",
+    name: "Flamingo",
+    desc: "I went to the Zoo and finally got to see a Flamingo with my own two eyes.",
+    tags: ["okra", "vegetable"],
+  },
+  {
+    id: 6,
+    src: "https://i.imgur.com/YvnWlaW.jpeg",
+    name: "Pineapple",
+    desc: "A pineapple on a table",
+    tags: ["pineapple", "fruit"],
+  },
+  {
+    id: 7,
+    src: "https://i.imgur.com/JTE1KcU.jpeg",
+    name: "Plant Window",
+    desc: "A window with plants",
+    tags: ["window", "plants"],
+  },
+  {
+    id: 8,
+    src: "https://i.imgur.com/3yUpjJV.jpeg",
+    name: "Pumpkin",
+    desc: "A pumpkin on a table",
+    tags: ["pumpkin", "vegetable"],
+  },
+  {
+    id: 9,
+    src: "https://i.imgur.com/Mn0LFu1.png",
+    name: "Sunset",
+    desc: "A sunset over the ocean",
+    tags: ["sunset", "ocean"],
+  },
+  {
+    id: 10,
+    src: "https://i.imgur.com/Z4rJL6X.jpeg",
+    name: "Sunset",
+    desc: "A sunset over the ocean",
+    tags: ["sunset", "ocean"],
+  },
+  {
+    id: 11,
+    src: "https://i.imgur.com/HurHDs0.jpeg",
+    name: "Sunset",
+    desc: "A sunset over the ocean",
+    tags: ["sunset", "ocean"],
+  },
+];
 
 export { GPhotos };
 
 import BiteSizedBingePoster from "./images/bitesizedbinge.webp";
+import { GalleryData } from "@app/about/Gallery";
 
 const OtherMultimediaProjectPictures = [BiteSizedBingePoster];
 
