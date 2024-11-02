edf2073a2ed687b69717063b13cd43ae1a685f1c
kalecream <sabrinamedwinter@gmail.com>
2024-11-02 10:59:37 -0500

    feat: add Text scramble component

---
+++ src/app/components/TextScramble.tsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useRef } from 'react';
+
+class TextScramble {
+  el: HTMLElement;
+  chars: string;
+  queue: Array<{ from: string; to: string; start: number; end: number; char?: string }>;
+  frameRequest?: number;
+  frame: number;
+  resolve?: () => void;
+
+  constructor(el: HTMLElement) {
+    this.el = el;
+    this.chars = "!<>-_\\/[]{}—=+*^?#________";
+    this.queue = [];
+    this.frame = 0;
+    this.update = this.update.bind(this);
+  }
+
+  setText(newText: string): Promise<void> {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise<void>((resolve) => (this.resolve = resolve));
+    this.queue = [];
+
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || '';
+      const to = newText[i] || '';
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end });
+    }
+
+    cancelAnimationFrame(this.frameRequest!);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+
+  update() {
+    let output = "";
+    let complete = 0;
+
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      const { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        const randomChar = char || this.randomChar();
+        output += `<span class="dud">${randomChar}</span>`;
+        this.queue[i].char = randomChar;
+      } else {
+        output += from;
+      }
+    }
+
+    this.el.innerHTML = output;
+
+    if (complete === this.queue.length) {
+      this.resolve && this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
+
+interface TextScrambleComponentProps {
+  phrases?: string[];
+}
+
+const TextScrambleComponent: React.FC<TextScrambleComponentProps> = ({ phrases}) => {
+
+  const defaultPhrases = [
+    "I am what I show up and do every day.",
+      "Nothing more,<br/> nothing less.",
+  ];
+
+  const textRef = useRef<HTMLDivElement | null>(null);
+  const fxRef = useRef<TextScramble | null>(null);
+  let counter = useRef(0);
+  const usedPhrases = phrases || defaultPhrases;
+
+  useEffect(() => {
+    if (textRef.current) {
+      fxRef.current = new TextScramble(textRef.current);
+
+      const next = () => {
+        fxRef.current
+          ?.setText(phrases[counter.current])
+          .then(() => setTimeout(next, 800));
+
+        counter.current = (counter.current + 1) % phrases.length;
+      };
+
+      next();
+    }
+
+    return () => cancelAnimationFrame(fxRef.current?.frameRequest!);
+  }, [phrases]);
+
+  return (
+    <h1 className="text" ref={textRef}>
+    </h1>
+  );
+};
+
+export default TextScrambleComponent;
