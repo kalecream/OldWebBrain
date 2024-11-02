19f34ff02d78d78b7aff8f687864419c28ea3496
kalecream <sabrinamedwinter@gmail.com>
2024-11-02 05:50:25 -0500

    chore: text update

--- src/app/professional/page.tsx
+++ src/app/professional/page.tsx
@@ -7,7 +7,7 @@ const ProfessionalPage = () => {
       <section>
         <h1>Services</h1>
         <p className="prose center" style={{ margin: "-1rem auto" }}>
-          If you're interested in any service offered on this page, please look at the portfolio for each below and{" "}
+          Look at the portfolio for each below and{" "}
           <Link href="mailto:sabrinamedwinter@gmail.com">reach out to me via email</Link>.
         </p>
         <ServiceCard />
