40f3b440c03ad7f0ccda00d4de53e851901b7675
kalecream <sabrinamedwinter@gmail.com>
2024-11-04 15:44:10 -0500

    styling: blur article around background

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -124,7 +124,7 @@ export default function Blog({ params }) {
 
         <br />
       </div>
-      <article>
+      <article className="blur">
         <CustomMDX source={post.content} />
       </article>
     </section>
