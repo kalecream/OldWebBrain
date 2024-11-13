5a94a7ef12e39fa59ea7c0b9a1a13e46975444c6
kalecream <sabrinamedwinter@gmail.com>
2024-11-13 07:19:22 -0500

    docs: improve code documentation

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "perf", "refactor", "style", "test", "build", "ops", "docs", "chore", "merge", "revert", "WIP"],
+    ],
+    "scope-enum": [2, "always", []], 
+    "subject-case": [2, "never", ["sentence-case"]],
+  },
+};
