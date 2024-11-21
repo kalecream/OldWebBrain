c1563a136e6c4c2b936f6bd0c36aea489ab0c86e
kalecream <sabrinamedwinter@gmail.com>
2024-11-21 03:37:47 -0500

    chore: add vetting link

--- src/app/kink/page.tsx
+++ src/app/kink/page.tsx
@@ -8,6 +8,9 @@ const KinkPage = () => {
         This will hold my notes on kink, BDSM, and other related topics as I explore solo BDSM and playdates.
       </p>
       <ol>
+      <li>
+          <Link href="kink/vetting">Vetting Playmates</Link>
+        </li>
         <li>
           <Link href="kink/shibari">Shibari</Link>
         </li>
