172d7bb88a9adb2cd0e98fe98158f30dd20c8097
kalecream <sabrinamedwinter@gmail.com>
2024-12-03 09:46:09 -0500

    styling: change colors

--- src/components/Guestbook/GuestBook.tsx
+++ src/components/Guestbook/GuestBook.tsx
@@ -8,44 +8,16 @@ import { GuestCard } from "./GuestCard";
 
 const formatDate = (date: string) => {
   noStore();
-  let currentDate = new Date().getTime();
   if (!date.includes("T")) {
     date = `${date}T00:00:00`;
   }
-  let targetDate = new Date(date).getTime();
-  let timeDifference = Math.abs(currentDate - targetDate);
-  let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  let hoursAgo = Math.floor(timeDifference / (1000 * 60 * 60));
-  let minutesAgo = Math.floor(timeDifference / (1000 * 60));
-  let secondsAgo = Math.floor(timeDifference / 1000);
 
   let fullDate = new Date(date).toLocaleString("en-us", {
     month: "short",
     day: "numeric",
   });
 
-  if (secondsAgo < 60 || minutesAgo < 1) {
-    return `just now`;
-  } else if (hoursAgo < 1) {
-    return `${minutesAgo} minutes ago`;
-  } else if (hoursAgo < 2) {
-    return `${hoursAgo} hour ago`;
-  } else if (hoursAgo < 24) {
-    return `${hoursAgo} hours ago`;
-  } else if (daysAgo < 2) {
-    return `${fullDate} (yesterday)`;
-  } else if (daysAgo < 7) {
-    return `${fullDate} (${daysAgo}d ago)`;
-  } else if (daysAgo < 30) {
-    const weeksAgo = Math.floor(daysAgo / 7);
-    return `${fullDate} (${weeksAgo}w ago)`;
-  } else if (daysAgo < 365) {
-    const monthsAgo = Math.floor(daysAgo / 30);
-    return `${fullDate} (${monthsAgo}mo ago)`;
-  } else {
-    const yearsAgo = Math.floor(daysAgo / 365);
-    return `${fullDate} (${yearsAgo}y ago)`;
-  }
+  return `${fullDate}`;
 };
 
 const Guestbook = () => {
@@ -97,8 +69,8 @@ const Guestbook = () => {
     <section>
       <h1>Guest Log</h1>
       <p className="prose" style={{ textAlign: "center" }}>
-        You were here. Leave your mark on a calling card. <br />
-        This guestbook was heavily inspired by <Link href={"https://eva.town/guestbook"}>eva.town</Link>
+        You were here. Leave your mark. <br />
+        This guestbook was heavily inspired by <Link href={"https://eva.town/guestbook"}>eva.town</Link>.
       </p>
       <div className={styles.notes}>
         <GuestCard id={0} href={""}>
@@ -106,16 +78,15 @@ const Guestbook = () => {
             <textarea
               value={note}
               name="content"
-              placeholder="* 150 characters left to make your mark."
+              placeholder="* 140 characters left"
               rows={4}
               cols={28}
-              maxLength={150}
+              maxLength={140}
               onChange={(e) => setNote(e.target.value)}
-              draggable={false}
               required
             ></textarea>
-            <input type="text" placeholder="* Name" required value={name} onChange={(e) => setName(e.target.value)} />
-            <input placeholder="? https://(url)" value={url} onChange={(e) => setUrl(e.target.value)} />
+            <input type="text" placeholder="* From?" required value={name} onChange={(e) => setName(e.target.value)} />
+            <input placeholder="https://(url)" value={url} onChange={(e) => setUrl(e.target.value)} />
             <button type="submit">Scribble</button>
           </form>
         </GuestCard>
@@ -125,14 +96,14 @@ const Guestbook = () => {
             const randomTranslateX = `${Math.random() * 24 - 12}px`;
 
             return (
-              <div
-                key={entry.id}
-                className={styles.note}
-                
-              >
-                <GuestCard id={i} href={""} style={{
-                  transform: `rotate(${randomRotation}) translateX(${randomTranslateX})`,
-                }}>
+              <div key={entry.id} className={styles.note}>
+                <GuestCard
+                  id={i}
+                  href={""}
+                  style={{
+                    transform: `rotate(${randomRotation}) translateX(${randomTranslateX})`,
+                  }}
+                >
                   <div className={styles.note_bg}>
                     <span className={styles.note_content}>{entry.note}</span>
                     <div className={styles.note_info}>
