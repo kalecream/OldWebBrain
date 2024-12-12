8fc59040accb3f4d65b975cb27db2c1d256f4aab
kalecream <sabrinamedwinter@gmail.com>
2024-12-12 11:52:22 -0500

    styling: change blog logo interaction

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,13 +1,11 @@
 "use client";
 import Link from "next/link";
 import style from "@styles/modules/nav.module.scss";
-import { usePathname } from "next/navigation";
 import Directory from "@data/directory";
 import Breadcrumb from "./Breadcrumb";
 import ThemeSwitch from "@components/navigation/theme-toggle";
 
 export function Navbar() {
-  const path = usePathname();
   return (
     <header id="header">
       <nav id="nav">
