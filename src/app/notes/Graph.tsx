0f03040edc1a53de7255ce7e2ae3bf0576e4a4be
Sab Medwinter <sabrinamedwinter@gmail.com>
2025-02-23 10:00:39 -0500

    fix: fixed graph components; build issues (#118)
    * del: remove unused tweet component
    
    * del: unused component
    
    * del: Removed broken component
    
    * build: refresh and upgrade packages
    
    * build: revert to compatible version of three
    
    * del: remove nused components
    
    * refactor: disable SSR for graph component
    
    * chore: rename imported component
    
    * refactor: add theme to component
    
    * styling: make wordgraph take up all width

--- src/app/notes/Graph.tsx
+++ src/app/notes/Graph.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import ForceGraph from "force-graph";
 
-export default function WordGraph({ data }) {
+export const Graph = ({ data }) => {
   const containerRef = useRef(null);
   const graphInstance = useRef(null);
   const systemPrefersDark = localStorage.getItem("theme") === "dark" ? true : false;
@@ -10,45 +10,12 @@ export default function WordGraph({ data }) {
     if (!graphInstance.current && containerRef.current) {
       graphInstance.current = new ForceGraph(containerRef.current)
         .graphData(data)
-        .autoPauseRedraw(false)
+        .nodeAutoColorBy("group")
+        .nodeRelSize(1.6)
         .nodeLabel((node) => `${node.id}`)
         .linkColor(() => {
           return systemPrefersDark ? `rgba(255,255,255,0.25)` : `rgba(255,0,0,0.2)`;
         })
-        .linkDirectionalParticles(2)
-        .linkDirectionalParticleWidth(1)
-        .nodeCanvasObject((node, ctx, globalScale) => {
-          const label = node.id;
-          const fontSize = 11 / globalScale;
-          ctx.font = `${fontSize}px Fira Code`;
-          ctx.fillStyle = `${systemPrefersDark ? `rgba(255,255,255,0.8)` : `rgba(25,0,0,0.8)`}`;
-          ctx.textAlign = "center";
-          ctx.textBaseline = "middle";
-          ctx.fillText(label.toString(), node.x, node.y);
-        })
-        .onNodeClick(function (node) {
-          window.location.href = `/notes/` + node.id.toString();
-        });
-    } else {
-      graphInstance.current.graphData(data);
-    }
-  }, [data, systemPrefersDark]);
-
-  return <div ref={containerRef} style={{ width: "60%", height: "800px", margin: "0 auto", overflow: "hidden" }} />;
-}
-
-export const Graph = ({ data }) => {
-  const containerRef = useRef(null);
-  const graphInstance = useRef(null);
-
-  useEffect(() => {
-    if (!graphInstance.current && containerRef.current) {
-      graphInstance.current = new ForceGraph(containerRef.current)
-        .graphData(data)
-        .nodeAutoColorBy("group")
-        .nodeRelSize(1.6)
-        .nodeLabel((node) => `${node.id}`)
-        .linkColor(() => "rgba(255,255,255,0.25)")
         .linkDirectionalParticles(4)
         .linkDirectionalParticleWidth(1)
         .onNodeClick(function (node) {
@@ -61,3 +28,5 @@ export const Graph = ({ data }) => {
 
   return <div ref={containerRef} style={{ width: "800px", height: "800px" }} />;
 };
+
+export default Graph;
