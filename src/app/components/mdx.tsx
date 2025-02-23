5dc36505436b48e0f968e6023a1512a3860b6fc1
kalecream <sabrinamedwinter@gmail.com>
2025-02-23 06:23:14 -0500

    del: remove nused components

--- src/app/components/mdx.tsx
+++ src/app/components/mdx.tsx
@@ -1,11 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import { highlight, Pre, type RawCode } from "codehike/code";
 import React from "react";
-import { LiveCode } from "./sandpack";
-import { codeToHtml } from "shiki";
-import type { BundledLanguage, BundledTheme } from "shiki";
-import { FaParagraph } from "react-icons/fa";
 
 function Table({ data }) {
   let headers = data.headers.map((header, index) => <th key={index}>{header}</th>);
@@ -32,7 +29,7 @@ function CustomLink(props) {
 
   if (href.startsWith("/")) {
     return (
-      <Link href={href} {...props}>
+      <Link className="internal-link" href={href} {...props}>
         {props.children}
       </Link>
     );
@@ -49,80 +46,9 @@ function RoundedImage(props) {
   return <Image alt={props.alt} {...props} />;
 }
 
-function Callout(props) {
-  return (
-    <div className="px-4 py-3 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm flex items-center text-neutral-900 dark:text-neutral-100 mb-8">
-      <div className="flex items-center w-4 mr-4">{props.emoji}</div>
-      <div className="w-full callout">{props.children}</div>
-    </div>
-  );
-}
-
-function ProsCard({ title, pros }) {
-  return (
-    <div className="border border-emerald-200 dark:border-emerald-900 bg-neutral-50 dark:bg-neutral-900 rounded-xl p-6 my-4 w-full">
-      <span>{`You might use ${title} if...`}</span>
-      <div className="mt-4">
-        {pros.map((pro) => (
-          <div key={pro} className="flex font-medium items-baseline mb-2">
-            <div className="h-4 w-4 mr-2">
-              <svg className="h-4 w-4 text-emerald-500" viewBox="0 0 24 24">
-                <g fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
-                  <path d="M22 4L12 14.01l-3-3" />
-                </g>
-              </svg>
-            </div>
-            <span>{pro}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-function ConsCard({ title, cons }) {
-  return (
-    <div className="border border-red-200 dark:border-red-900 bg-neutral-50 dark:bg-neutral-900 rounded-xl p-6 my-6 w-full">
-      <span>{`You might not use ${title} if...`}</span>
-      <div className="mt-4">
-        {cons.map((con) => (
-          <div key={con} className="flex font-medium items-baseline mb-2">
-            <div className="h-4 w-4 mr-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-4 w-4 text-red-500"
-              >
-                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-              </svg>
-            </div>
-            <span>{con}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-type Props = {
-  children: string;
-  lang?: BundledLanguage;
-  theme?: BundledTheme;
-  filename?: string;
-};
-
-const Code = async ({ children, lang = "javascript", theme = "nord", filename = ".txt" }: Props) => {
-  let codeHTML = await codeToHtml(children, { lang, theme });
-  return (
-    <div className="code">
-      {/* <div className='filename'>
-				{filename && <caption>{filename}</caption>}
-			</div> */}
-      <code dangerouslySetInnerHTML={{ __html: codeHTML }} />
-    </div>
-  );
+const Code = async ({ codeblock }: { codeblock: RawCode }) => {
+  const highlighted = await highlight(codeblock, "github-dark")
+  return <Pre code={highlighted}  />
 };
 
 function slugify(str) {
@@ -154,13 +80,9 @@ function createHeading(level) {
   };
 }
 
-const paragraph = ({children}) => {
-  return (
-    <p className="blur">
-      {children}
-    </p>
-  )
-}
+const paragraph = ({ children }) => {
+  return <p className="blur">{children}</p>;
+};
 
 let components = {
   h1: createHeading(1),
@@ -172,12 +94,8 @@ let components = {
   p: paragraph,
   Image: RoundedImage,
   a: CustomLink,
-  Callout,
-  ProsCard,
-  ConsCard,
   code: Code,
-  Table,
-  LiveCode,
+  Table
 };
 
 export function CustomMDX(props) {
