e43c04bd980865aef52c641c824bebf44f00f53f
Sab Medwinter <sabrinamedwinter@gmail.com>
2025-03-02 12:39:03 -0500

    refactor: revert to Shiki codeblocks (#119)
    * refactor: convert next config to modules
    
    * refactor: revert to shiki codeblocks
    
    * del: remove codehike

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,28 @@
+import withMDX from "@next/mdx";
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
 /** @type {import('next').NextConfig} */
-const withMDX = require("@next/mdx")({
+const mdxConfig = withMDX({
   extension: /\.mdx?$/,
   jsx: true,
   options: {
     remarkPlugins: [[]],
-    recmaPlugins: [],
+    recmaPlugins: [[]],
+    jsx: true,
   },
 });
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
+/** @type {import('next').NextConfig} */
+const bundleAnalyzerConfig = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure pageExtensions to include md and mdx
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  // Optionally, add any other Next.js config below
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    // path: '/img/',
     formats: ["image/webp"],
     remotePatterns: [
       { hostname: "cdn.sanity.io" },
@@ -34,14 +36,14 @@ const nextConfig = {
     includePaths: ["src/app/styles"],
   },
   typescript: {
-    // Set this to false if you want production builds to abort if there's type errors
     ignoreBuildErrors: process.env.VERCEL_ENV === "production",
   },
   eslint: {
-    /// Set this to false if you want production builds to abort if there's lint errors
     ignoreDuringBuilds: process.env.VERCEL_ENV === "production",
   },
 };
 
 // Merge MDX config with Next.js config
-module.exports = withBundleAnalyzer(withMDX(nextConfig));
+const config = bundleAnalyzerConfig(mdxConfig(nextConfig));
+
+export default config;
