ece7351b470fd274a116505c45b522a9963398d2
kalecream <sabrinamedwinter@gmail.com>
2025-06-16 05:35:36 -0500

    chore: update reccomended videos

--- src/data/youtube.ts
+++ src/data/youtube.ts
@@ -2,5 +2,9 @@ const YT = [
     {
         url: "https://www.youtube.com/watch?v=srMEoe_5y6g",
         title: "Why Recipes are holding you back from learning how to cook x Ethan Chlebowski"
+    },
+    {
+        url: "https://youtu.be/QCQ3KC81QHI?si=WTwcTJRG4FKKeyuw",
+        title: "[Making Yogurt from scratch using chili peppers or lemons] I Discovered A NEW WAY To Make Yogurt! x Milkslinger",
     }
 ]
