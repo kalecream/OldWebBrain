977735ab1a60efc263104eb26118486a8c9630c7
Sab Medwinter <sabrinamedwinter@gmail.com>
2025-10-16 22:45:38 +0000

    chore: update social media

--- src/components/navigation/socialmedia.ts
+++ src/components/navigation/socialmedia.ts
@@ -1,25 +1,10 @@
-import { FaTwitter, FaGithub, FaInstagram, FaTelegram, FaMastodon } from "react-icons/fa6";
+import {  FaGithub, FaTelegram } from "react-icons/fa6";
 
 export const MediaProfiles = [
-  {
-    name: "Twitter",
-    url: "https://twitter.com/yunghigue",
-    icon: FaTwitter,
-  },
   {
     name: "Github",
     url: "https://www.github.com/kalecream",
     icon: FaGithub,
-  },
-  {
-    name: "Instagram",
-    url: "https://www.instagram.com/yunghigue",
-    icon: FaInstagram,
-  },
-  {
-    name: "Mastodon",
-    url: "https://mstdn.social/@KaleCream",
-    icon: FaMastodon,
   },
   {
     name: "Telegram",
