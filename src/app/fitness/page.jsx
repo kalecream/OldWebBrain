8ee6be79ea05654cc1c1e88b5648567dddf7b926
Sab Medwinter <sabrinamedwinter@gmail.com>
2026-01-18 10:44:24 -0500

    feat: enhance C25KPage with helpful resources and updated content

--- src/app/fitness/page.jsx
+++ src/app/fitness/page.jsx
@@ -76,26 +76,38 @@ const C25KPage = () => {
 
   return (
     <section>
-      <h1>Couch to 5K</h1>
       <div className="flex column">
         <Suspense fallback={<Skeleton count={1} />}>
           <SplitChart />
         </Suspense>
       </div>
       <p className="prose glassmorphic" style={{ marginTop: "2rem", marginBottom: "3rem" }}>
-        I'm using "<Link href="https://zrx.app/">Zombies, Run 5K Training</Link>" to do an 8 week program of the Couch
+        I'm trying to walk more then return to using "<Link href="https://zrx.app/">Zombies, Run 5K Training</Link>" to do an 8 week program of the Couch
         to 5KM challenge. I attempted this briefly before, but I have never made it fully through and would like to
         improve my running ability.
       </p>
-      <p className="prose glassmorphic" style={{ marginTop: "-2rem" }}>
-        Something that really helped was looking up YouTube videos on{" "}
-        <Link href="https://www.youtube.com/watch?v=_kGESn8ArrU&pp=ygUKaG93IHRvIHJ1bg%3D%3D">how to run properly</Link>.
-        For some reason, I thought running forms would be instinctive. It is not. Similarly, I had to{" "}
-        <Link href="https://www.youtube.com/watch?v=B9ie7aRTCnE&pp=ygUcaG93IHRvIGJyZWF0aGUgd2hpbGUgcnVubmluZw%3D%3D">
-          learn to breathe
-        </Link>{" "}
-        as evolution failed me by making my instinct holding my breath.
-      </p>
+
+      <h2 className="prose glassmorphic">Helpful Resources</h2>
+      <ol className="prose glassmorphic">
+        <li>
+          <Link href="https://www.zombiesrungame.com/">Zombies, Run!</Link> - An app that makes running more engaging by
+          incorporating a story and missions.
+        </li>
+        <li>
+          <Link href="https://www.c25k.com/">Couch to 5K</Link> - A popular program designed to help beginners gradually build up
+          their running ability.
+        </li>
+        <li>
+          <Link href="https://www.youtube.com/watch?v=_kGESn8ArrU&pp=ygUKaG93IHRvIHJ1bg%3D%3D">How to run properly</Link>{" "} - A video tutorial on proper running form and techniques to prevent injuries.
+        </li>
+        <li>
+          <Link href="https://www.youtube.com/watch?v=B9ie7aRTCnE&pp=ygUcaG93IHRvIGJyZWF0aGUgd2hpbGUgcnVubmluZw%3D%3D">How To Breathe While Running
+          </Link>{" "}- A video tutorial on proper breathing techniques while running as evolution failed me by making my instinct holding my breath.
+        </li>
+        <li>
+          <Link href="https://docs.google.com/spreadsheets/d/1a8tlZ-zbF695HA3Lmm20OIYeYYxo1lmUOczUXKLoL4s/edit?gid=1833143925#gid=1833143925">Spreadsheet of all Bodyweight Fitness Progressions</Link> - A comprehensive spreadsheet that tracks progressions for various bodyweight exercises, useful for tracking strength training alongside running.
+        </li>
+      </ol>
     </section>
   );
 };
