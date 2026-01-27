a95c056a7d8a86d968eb9fd43f79bafe1a31cec7
Sab Medwinter <sabrinamedwinter@gmail.com>
2026-01-27 13:10:16 -0500

    fix: remove Footer component from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <SpeedInsights />
             <BackToTop />
           </main>
-          <Footer />
         </ThemeProvider>
       </body>
     </html>
