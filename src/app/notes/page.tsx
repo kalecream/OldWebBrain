1a86aae8b07ddc3338356d8320015a9c19d4fea7
Sab Medwinter <sabrinamedwinter@gmail.com>
2026-01-26 20:24:36 -0500

    refactor: restructure NotesPage layout and improve styling in Garden module

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -19,43 +19,41 @@ export const NotesPage = () => {
     .sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime());
 
   return (
-    <div className={garden.wrapper}>
-      <section style={{ padding: 0, margin: "auto", marginTop: "3rem"  }}>
-        <input
-          type="text"
-          placeholder="Search notes..."
-          className="center blur"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          style={{
-            maxWidth: "65rem",
-            width: "100%",
-            height: "2.5rem",
-            padding: "1rem",
-            backgroundColor: "transparent",
-            color: "var(--textcolor)",
-            border: "var(--border)",
-            borderRadius: "var(--borderRadius)",
-          }}
-        />
-        <ul className={garden["notes-list"]}>
-          <Suspense>
-            {filteredNotes.length > 0 ? (
-              filteredNotes.map(({ slug, data }) => (
-                <li key={slug} className="garden.note">
-                  <Link href={`/notes/${slug}`} className="">
-                    <small className={garden["note-title"]}>{data?.title || slug.split("-").join(" ")}</small>
-                   <small className={garden["note-date"]}>{data?.date?.toLocaleString() || ""}</small>
-                  </Link>
-                </li>
-              ))
-            ) : (
-              <p className="text-center center">...Loading Notes...</p>
-            )}
-          </Suspense>
-        </ul>
-      </section>
-    </div>
+    <section className={garden.wrapper}>
+      <input
+        type="text"
+        placeholder="Search notes..."
+        className="center blur"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        style={{
+          maxWidth: "65rem",
+          width: "100%",
+          height: "2.5rem",
+          padding: "1rem",
+          backgroundColor: "transparent",
+          color: "var(--textcolor)",
+          border: "var(--border)",
+          borderRadius: "var(--borderRadius)",
+        }}
+      />
+      <ul className={garden["notes-list"]}>
+        <Suspense>
+          {filteredNotes.length > 0 ? (
+            filteredNotes.map(({ slug, data }) => (
+              <li key={slug} className="garden.note">
+                <Link href={`/notes/${slug}`} className="">
+                  <small className={garden["note-title"]}>{data?.title || slug.split("-").join(" ")}</small>
+                  <small className={garden["note-date"]}>{data?.date?.toLocaleString() || ""}</small>
+                </Link>
+              </li>
+            ))
+          ) : (
+            <p className="text-center center">...Loading Notes...</p>
+          )}
+        </Suspense>
+      </ul>
+    </section>
   );
 };
 
