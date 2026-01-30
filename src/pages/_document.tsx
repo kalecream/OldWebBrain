fdcd9bce7536cc2d3f7cb3485fbe8f75db38f03c
Sab Medwinter <sabrinamedwinter@gmail.com>
2026-01-30 06:42:27 -0500

    Refactor code structure for improved readability and maintainability

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html lang="en">
+      <Head>
+        <link 
+          rel="preload" 
+          href="/assets/images/light-grey-terrazzo.webp" 
+          as="image"
+          fetchPriority="high"
+        />
+         <link 
+          rel="preload" 
+          href="/assets/images/stars.gif" 
+          as="image"
+          fetchPriority="high"
+        />
+      </Head>
       <Head />
       <body>
         <Main />
