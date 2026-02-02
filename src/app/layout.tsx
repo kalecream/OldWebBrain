25605f7ebc6fa16bcef00be9fbb6e1eeab0a0d24
Sab Medwinter <sabrinamedwinter@gmail.com>
2026-02-02 13:24:50 -0500

    build: move to app router completely

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from "next";
 import { Navbar } from "./components/nav";
 import { Footer } from "./components/footer";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+// import { SpeedInsights } from "@vercel/speed-insights/next";
 import { BackToTop } from "@components/_basics/BackToTop";
 import { ThemeProvider } from "./context/Theme";
 import "@styles/global.scss";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://yunghigue.com"),
@@ -32,9 +33,6 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      // noai: true,
-      // noimageai: true
-      // TODO: check when this becomes available in nextjs
       "max-video-preview": -1,
       "max-image-preview": "large",
       "max-snippet": -1,
@@ -54,14 +52,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head/>
+      <Head>
+        <link rel="preload" href="/assets/images/light-grey-terrazzo.webp" as="image" fetchPriority="high" />
+        <link rel="preload" href="/assets/images/stars.gif" as="image" fetchPriority="high" />
+      </Head>
       <body>
         <ThemeProvider>
           <Navbar />
           <main>
             {children}
             <Analytics />
-            <SpeedInsights />
+            {/* <SpeedInsights /> */}
             <BackToTop />
           </main>
         </ThemeProvider>
