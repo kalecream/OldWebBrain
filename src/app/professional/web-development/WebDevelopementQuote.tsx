19758902071049009d0e21c3833e6c2fbdb0b85e
Sab Medwinter <sabrinamedwinter@gmail.com>
2026-02-12 06:36:16 -0500

    chore: change file import path

--- src/app/professional/web-development/WebDevelopementQuote.tsx
+++ src/app/professional/web-development/WebDevelopementQuote.tsx
@@ -1,6 +1,6 @@
 "use client";
 import styled from "styled-components";
-import { Projects } from "@components/Projects/ProjectsData";
+import { Projects } from "@components/Projects/projectsData";
 
 // TODO: do next
 
