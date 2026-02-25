b1fd08e0c309e2ffbebc7b288b3630fcbdbe914e
Sab Medwinter <sabrinamedwinter@gmail.com>
2026-02-25 15:34:05 -0500

    refactor: refine blog post section

--- src/app/blog/BlogPosts.tsx
+++ src/app/blog/BlogPosts.tsx
@@ -1,8 +1,8 @@
-import { useMemo } from "react";
 import Link from "next/link";
 import { getBlogPosts } from "../db/blog";
-import blog from "./BlogSummary.module.scss";
+import b from "./BlogSummary.module.scss";
 import NineGridGallery from "./Gallery";
+
 
 type Year = {
   date: string;
@@ -12,41 +12,29 @@ type Year = {
 type Month = {
   date: string;
   name: string;
-  posts: any;
+  posts: any[];
 };
 
-const BlogDateSplit = (posts) => {
+const BlogDateSplit = (posts: any[]): Year[] => {
   let years: Year[] = [];
   const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   posts.forEach((post) => {
     const [yearDate, monthDate, dayDate] = post.metadata.date.split(" ")[0].split("-");
 
-    let year: Year | undefined = years.find((year) => year.date === yearDate);
+    let year = years.find((y) => y.date === yearDate);
     if (!year) {
-      year = {
-        date: yearDate,
-        months: [],
-      };
+      year = { date: yearDate, months: [] };
       years.push(year);
     }
 
-    let month = year.months.find((month) => month.date === monthDate);
+    let month = year.months.find((m) => m.date === monthDate);
     if (!month) {
-      const monthIndex = parseInt(monthDate) - 1;
-      const monthName = monthNames[monthIndex];
-      month = {
-        date: monthDate,
-        name: monthName,
-        posts: [],
-      };
+      month = { date: monthDate, name: monthNames[parseInt(monthDate) - 1], posts: [] };
       year.months.push(month);
     }
 
-    month.posts.push({
-      ...post,
-      date: dayDate,
-    });
+    month.posts.push({ ...post, date: dayDate });
   });
 
   years
@@ -62,47 +50,56 @@ const BlogDateSplit = (posts) => {
   return years;
 };
 
-export default async function BlogPosts() {
-  let posts = getBlogPosts();
-  let years = useMemo(() => BlogDateSplit(posts), [posts]);
+export default function BlogPosts() {
+  const posts = getBlogPosts();
+  const years = BlogDateSplit(posts);
 
   return (
-    <section style={{ marginTop: "3rem" }}>
-      {years.map((year) => (
-        <ul key={year.date} className={blog.container}>
-          <li key={year.date}>
-            <ul>
-              {year.months.map((month, i) => (
-                <div className={blog.month_lump} key={i}>
-                  <li className={blog.nolist} key={i}>
-                    <h2 className={blog.month}>
-                      <span>{month.name}</span> <span>{i === 0 && year.date}</span>
-                    </h2>
-                    <ul>
-                      {month.posts.map((post) => (
-                        <li key={post.slug} className={blog.post + ` no-marker h-entry`}>
-                          <Link className={blog.link + `  u-url`} href={`/blog/${post.slug}`}>
-                            <div className={blog.list__section}>
-                              <div>
-                                <span className={blog.list__date + ` dt-published`}>
+    <>
+      <section className={b["blog-root"]}>
+        {years.map((year) => (
+          <ul key={year.date} className={b["blog-year-block"]}>
+            <li>
+              <ul style={{ padding: 0, margin: 0 }}>
+                {year.months.map((month, i) => (
+                  <div className={b["blog-month-lump"]} key={`${year.date}-${month.date}`}>
+                    <li className={b["blog-month-card"]}>
+                      <h2 className={b["blog-month-heading"]}>
+                        <span className={b["blog-month-name"]}>{month.name}</span>
+                        {i === 0 && <span className={b["blog-year-label"]}>{year.date}</span>}
+                      </h2>
+
+                      <ul className={b["blog-post-list"]}>
+                        {month.posts.map((post) => (
+                          <li key={post.slug} className={b["blog-post-item"] + " no-marker h-entry"}>
+                            <Link className={b["blog-post-link"] + " u-url"} href={`/blog/${post.slug}`}>
+                              <div className={b["blog-post-row"]}>
+                                <span className={b["blog-post-day"] + " dt-published"}>
                                   {post.metadata.date.slice(-2)}
                                 </span>
-                                <h3 className={blog.list__title + ` p-name`}>{post.metadata.title}</h3>
+                                <h3 className={b["blog-post-title"] + " p-name"}>
+                                  {post.metadata.title}
+                                </h3>
                               </div>
-                              <p className={blog.list__description + ` e-content`}>{post.metadata.description}</p>
-                            </div>
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
-                  <NineGridGallery year={year.date} month={month.date} />
-                </div>
-              ))}
-            </ul>
-          </li>
-        </ul>
-      ))}
-    </section>
+                              {post.metadata.description && (
+                                <p className={b["blog-post-description"] + " e-content"}>
+                                  {post.metadata.description}
+                                </p>
+                              )}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+
+                    <NineGridGallery year={year.date} month={month.date} />
+                  </div>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        ))}
+      </section>
+    </>
   );
 }
